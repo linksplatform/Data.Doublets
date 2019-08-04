e5d85344a9171f38f3910286626c8fb9e9d5d871
Константин Дьяченко <konard@me.com>
2019-08-04 16:39:16 +0300

    Another attempt to fix.

--- fmt.sh
+++ fmt.sh
@@ -10,20 +10,8 @@ echo """
 \\usepackage[T1]{fontenc}
 \\usepackage[T2A]{fontenc}
 \\usepackage[gray]{xcolor}
+\\usepackage{fvextra}
 \\usepackage{minted}
-
-\\makeatletter
-\\let\\xUTFviii@two@octets\\UTFviii@two@octets
-
-\\def\\UTFviii@two@octets#1#2{%
-\\ifx\\FancyVerbBreakAnywhereBreak#2%
-\\expandafter\\xUTFviii@two@octets\\expandafter#1%
-\\else
-\\xUTFviii@two@octets#1#2%
-\\fi
-}
-\\makeatother
-
 \\usepackage{multicol}
 \\usepackage{makeidx}
 \\usepackage[columns=2]{idxlayout}
@@ -36,9 +24,6 @@ echo """
 \\titlespacing\\section{0mm}{0mm}{0mm}
 \\DeclareUnicodeCharacter{221E}{\\ensuremath{\\infty}}
 \\DeclareUnicodeCharacter{FFFD}{\\ensuremath{ }}
-\\DeclareUnicodeCharacter{D86E}{ }
-\\DeclareUnicodeCharacter{DEC8}{ }
-\\DeclareUnicodeCharacter{2BAC8}{ }
 \\usepackage{fancyhdr}
 \\pagestyle{fancy}
 \\fancyhf{}
@@ -47,12 +32,6 @@ echo """
 \\renewcommand{\\footrulewidth}{0mm}
 \\renewcommand{\\baselinestretch}{0.7}
 \\begin{document}
-
-\\newminted{csharp}{
-    breaklines,
-    breakanywhere
-}
-
 \\sf
 \\noindent{\\Large LinksPlatform's Platform.Data.Doublets Class Library}
 \\begin{multicols}{2}
