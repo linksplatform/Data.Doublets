e56586b45119fc1137c6d91c8d5ddd2a3e94c535
Константин Дьяченко <konard@me.com>
2019-08-18 11:49:37 +0300

    Refactoring.

--- Platform.Data.Doublets.Tests/UnaryNumberConvertersTests.cs
+++ Platform.Data.Doublets.Tests/UnaryNumberConvertersTests.cs
@@ -1,6 +1,6 @@
-﻿using Platform.Data.Doublets.Converters;
+﻿using Xunit;
 using Platform.Random;
-using Xunit;
+using Platform.Data.Doublets.Converters;
 
 namespace Platform.Data.Doublets.Tests
 {
@@ -12,30 +12,21 @@ namespace Platform.Data.Doublets.Tests
             using (var scope = new TempLinksTestScope())
             {
                 const int N = 10;
-
                 var links = scope.Links;
-
                 var meaningRoot = links.CreatePoint();
                 var one = links.CreateAndUpdate(meaningRoot, links.Constants.Itself);
-
                 var powerOf2ToUnaryNumberConverter = new PowerOf2ToUnaryNumberConverter<ulong>(links, one);
-
                 var toUnaryNumberConverter = new AddressToUnaryNumberConverter<ulong>(links, powerOf2ToUnaryNumberConverter);
-
                 var random = new System.Random(0);
-
                 ulong[] numbers = new ulong[N];
                 ulong[] unaryNumbers = new ulong[N];
-
                 for (int i = 0; i < N; i++)
                 {
                     numbers[i] = random.NextUInt64();
                     unaryNumbers[i] = toUnaryNumberConverter.Convert(numbers[i]);
                 }
-
                 var fromUnaryNumberConverterUsingOrOperation = new UnaryNumberToAddressOrOperationConverter<ulong>(links, powerOf2ToUnaryNumberConverter);
                 var fromUnaryNumberConverterUsingAddOperation = new UnaryNumberToAddressAddOperationConverter<ulong>(links, one);
-
                 for (int i = 0; i < N; i++)
                 {
                     Assert.Equal(numbers[i], fromUnaryNumberConverterUsingOrOperation.Convert(unaryNumbers[i]));
