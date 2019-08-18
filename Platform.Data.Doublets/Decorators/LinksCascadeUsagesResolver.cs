19a7f16d15b6a7790e741889e5027f4a5fb5b4eb
Константин Дьяченко <konard@me.com>
2019-08-18 12:29:50 +0300

    LinksCascadeUsagesResolver must be used in conjunction with NonNullContentsLinkDeletionResolver, no need to repeat logic.

--- Platform.Data.Doublets/Decorators/LinksCascadeUsagesResolver.cs
+++ Platform.Data.Doublets/Decorators/LinksCascadeUsagesResolver.cs
@@ -1,15 +1,15 @@
 ﻿namespace Platform.Data.Doublets.Decorators
 {
+    /// <remarks>
+    /// <para>Must be used in conjunction with NonNullContentsLinkDeletionResolver.</para>
+    /// <para>Должен использоваться вместе с NonNullContentsLinkDeletionResolver.</para>
+    /// </remarks>
     public class LinksCascadeUsagesResolver<TLink> : LinksDecoratorBase<TLink>
     {
         public LinksCascadeUsagesResolver(ILinks<TLink> links) : base(links) { }
 
         public override void Delete(TLink linkIndex)
         {
-            if (!Links.AreValuesReset(linkIndex))
-            {
-                Links.ResetValues(linkIndex);
-            }
             this.DeleteAllUsages(linkIndex);
             Links.Delete(linkIndex);
         }
