e1e591a1859d11384e00832f4e3f270aa6bb268b
Константин Дьяченко <konard@me.com>
2019-08-19 19:59:32 +0300

    Added raw numbers converters (using Hybrid class).

---
+++ Platform.Data.Doublets/Numbers/Raw/RawNumberToAddressConverter.cs
@@ -0,0 +1,12 @@
+﻿using Platform.Interfaces;
+using Platform.Numbers;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Doublets.Numbers.Raw
+{
+    public class RawNumberToAddressConverter<TLink> : IConverter<TLink>
+    {
+        public TLink Convert(TLink source) => (Integer<TLink>)new Hybrid<TLink>(source).AbsoluteValue;
+    }
+}
