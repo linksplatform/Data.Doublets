bebbe59a7faa0db1df394201dfa3f3569bc18fa3
Konstantin Diachenko <drakonard@gmail.com>
2019-09-22 08:54:47 +0300

    By default all added already, and all might be contained.

--- Platform.Data.Doublets/Sequences/Indexes/Unindex.cs
+++ Platform.Data.Doublets/Sequences/Indexes/Unindex.cs
@@ -6,7 +6,7 @@ namespace Platform.Data.Doublets.Sequences.Indexes
 {
     public class Unindex<TLink> : ISequenceIndex<TLink>
     {
-        public virtual bool Add(IList<TLink> sequence) => true;
+        public virtual bool Add(IList<TLink> sequence) => false;
 
         public virtual bool MightContain(IList<TLink> sequence) => true;
     }
