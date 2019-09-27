b03363c52750cfa245c25273364614a9a3132fe0
Константин Дьяченко <konard@me.com>
2019-09-27 11:34:37 +0300

    Added test for LinksConstants and Hybrid interaction.

---
+++ Platform.Data.Doublets.Tests/LinksConstantsTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Platform.Data.Doublets.Tests
+{
+    public static class LinksConstantsTests
+    {
+        [Fact]
+        public static void ExternalReferencesTest()
+        {
+            LinksConstants<ulong> constants = new LinksConstants<ulong>((1, long.MaxValue), (long.MaxValue + 1UL, ulong.MaxValue));
+
+            //var minimum = new Hybrid<ulong>(0, isExternal: true);
+            var minimum = new Hybrid<ulong>(1, isExternal: true);
+            var maximum = new Hybrid<ulong>(long.MaxValue, isExternal: true);
+
+            Assert.True(constants.IsExternalReference(minimum));
+            Assert.True(constants.IsExternalReference(maximum));
+        }
+    }
+}
