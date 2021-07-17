fded7427ab43743b638d7489eb786652313ef0fb
FreePhoenix <freephoenix888@gmail.com>
2021-07-17 15:23:24 +0600

    Add bug test and output

---
+++ csharp/Platform.Data.Doublets.Tests/Uint64LinksExtensionsTests.cs
@@ -0,0 +1,50 @@
+using Platform.Data.Doublets.Memory;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Numbers.Raw;
+using Platform.Memory;
+using Platform.Numbers;
+using Xunit;
+using Xunit.Abstractions;
+using TLink = System.UInt64;
+
+namespace Platform.Data.Doublets.Tests
+{
+    public class Uint64LinksExtensionsTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Uint64LinksExtensionsTests(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+        public static ILinks<TLink> CreateLinks() => CreateLinks<TLink>(new Platform.IO.TemporaryFile());
+
+        public static ILinks<TLink> CreateLinks<TLink>(string dataDBFilename)
+        {
+            var linksConstants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
+            return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(dataDBFilename), UnitedMemoryLinks<TLink>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
+        }
+        [Fact]
+        public void BugTest()
+        {
+            // Trying to create link "(43:(7:1 7) (42:(10:1 10) (41:7 (40:(9:1 9) 0))))"
+            ILinks<TLink> links = CreateLinks();
+            TLink zero = default;
+            var one = Arithmetic.Increment(zero);
+            var markerIndex = one;
+            var meaningRoot = links.GetOrCreate(markerIndex, markerIndex);
+            var valueMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex)); // 7
+            var arrayMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex)); // 10
+            var numberMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex)); // 9
+            AddressToRawNumberConverter<TLink> addressToNumberConverter = new();
+            // RawNumberToAddressConverter<TLink> numberToAddressConverter = new();
+            var numberAddress = addressToNumberConverter.Convert(1); // Address of 0
+            var numberLink = links.GetOrCreate(numberMarker, numberAddress); // 40
+            var numberValue = links.GetOrCreate(valueMarker, numberLink); // 41
+            var array = links.GetOrCreate(arrayMarker, numberValue);
+            var arrayValue = links.GetOrCreate(valueMarker, array);
+            var output = ((ILinks<ulong>)(object)links).FormatStructure((ulong)(object)arrayValue, link => link.IsFullPoint(), true);
+            _output.WriteLine(output);
+        }
+    }
+}
