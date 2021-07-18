595ea9df342e3646e081b2bfbddbf80e94cc7e28
FreePhoenix <freephoenix888@gmail.com>
2021-07-18 16:08:48 +0600

    Update test

--- csharp/Platform.Data.Doublets.Tests/DefaultSequenceAppenderTests.cs
+++ csharp/Platform.Data.Doublets.Tests/DefaultSequenceAppenderTests.cs
@@ -33,14 +33,14 @@ namespace Platform.Data.Doublets.Tests
         public class ValueCriterionMatcher<TLink> : ICriterionMatcher<TLink>
         {
             public readonly ILinks<TLink> Links;
-            public readonly TLink ValueMarker;
-            public ValueCriterionMatcher(ILinks<TLink> links, TLink valueMarker)
+            public readonly TLink Marker;
+            public ValueCriterionMatcher(ILinks<TLink> links, TLink marker)
             {
                 Links = links;
-                ValueMarker = valueMarker;
+                Marker = marker;
             }
 
-            public bool IsMatched(TLink link) => EqualityComparer<TLink>.Default.Equals(Links.GetSource(link), ValueMarker);
+            public bool IsMatched(TLink link) => EqualityComparer<TLink>.Default.Equals(Links.GetSource(link), Marker);
         }
 
         [Fact]
@@ -48,26 +48,18 @@ namespace Platform.Data.Doublets.Tests
         {
             ILinks<TLink> links = CreateLinks();
             TLink zero = default;
-            var one = Arithmetic.Increment(zero);
-            var markerIndex = one;
+            var markerIndex = Arithmetic.Increment(zero);
             var meaningRoot = links.GetOrCreate(markerIndex, markerIndex);
-            var valueMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex));
-            var numberMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex));
-            var arrayMarker = links.GetOrCreate(meaningRoot, Arithmetic.Increment(ref markerIndex));
-            AddressToRawNumberConverter<TLink> addressToNumberConverter = new();
-            var numberOneAddress = addressToNumberConverter.Convert(1);
-            var numberOneLink = links.GetOrCreate(numberMarker, numberOneAddress);
-            var array = links.GetOrCreate(arrayMarker, numberOneLink);
-            var arrayValue = links.GetOrCreate(valueMarker, array);
-            var numberTwoAddress = addressToNumberConverter.Convert(2);
-            var numberTwoLink = links.GetOrCreate(numberMarker, numberTwoAddress);
-            var appendant = links.GetOrCreate(valueMarker, numberTwoLink);
-            var equalityComparer = EqualityComparer<TLink>.Default;
-            ValueCriterionMatcher<TLink> valueCriterionMatcher = new(links, valueMarker);
+            var sequence = links.Create();
+            sequence = links.Update(sequence, meaningRoot, sequence);
+            var appendant = links.Create();
+            appendant = links.Update(appendant, meaningRoot, appendant);
+            ValueCriterionMatcher<TLink> valueCriterionMatcher = new(links, meaningRoot);
             DefaultSequenceRightHeightProvider<ulong> defaultSequenceRightHeightProvider = new(links, valueCriterionMatcher);
             DefaultSequenceAppender<TLink> defaultSequenceAppender = new(links, new DefaultStack<ulong>(), defaultSequenceRightHeightProvider);
-            var newArray = defaultSequenceAppender.Append(arrayValue, appendant);
+            var newArray = defaultSequenceAppender.Append(sequence, appendant);
             var output = links.FormatStructure(newArray, link => link.IsFullPoint(), true);
+            Assert.Equal("(4:(2:1 2) (3:1 3))", output);
         }
     }
 }
