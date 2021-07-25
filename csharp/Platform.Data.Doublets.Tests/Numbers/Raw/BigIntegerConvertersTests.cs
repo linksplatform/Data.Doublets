76c1860c05e2d4d1077131926f4c40f36fb88b8c
FreePhoenix <freephoenix888@gmail.com>
2021-07-25 23:01:15 +0600

    Add draft

---
+++ csharp/Platform.Data.Doublets.Tests/Numbers/Raw/BigIntegerConvertersTests.cs
@@ -0,0 +1,35 @@
+using System.Numerics;
+using Platform.Data.Doublets.Memory;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Numbers.Raw;
+using Platform.Data.Numbers.Raw;
+using Platform.Memory;
+using Xunit;
+using TLink = System.UInt64;
+
+namespace Platform.Data.Doublets.Tests.Numbers.Raw
+{
+    public class BigIntegerConvertersTests
+    {
+        public ILinks<TLink> CreateLinks() => CreateLinks<TLink>(new Platform.IO.TemporaryFile());
+
+        public ILinks<TLink> CreateLinks<TLink>(string dataDBFilename)
+        {
+            var linksConstants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
+            return new UnitedMemoryLinks<TLink>(new FileMappedResizableDirectMemory(dataDBFilename), UnitedMemoryLinks<TLink>.DefaultLinksSizeStep, linksConstants, IndexTreeType.Default);
+        }
+        [Fact]
+        public void Test()
+        {
+            var links = CreateLinks();
+            BigInteger bigInt = new(1);
+            AddressToRawNumberConverter<TLink> addressToRawNumberConverter = new();
+            RawNumberToAddressConverter<TLink> numberToAddressConverter = new();
+            BigIntegerToRawNumberSequenceConverter<TLink> bigIntegerToRawNumberSequenceConverter = new(links, addressToRawNumberConverter);
+            RawNumberSequenceToBigIntegerConverter<TLink> rawNumberSequenceToBigIntegerConverter = new(links, numberToAddressConverter);
+            var bigIntSequence = bigIntegerToRawNumberSequenceConverter.Convert(bigInt);
+            var bigIntFromSequence = rawNumberSequenceToBigIntegerConverter.Convert(bigIntSequence);
+            Assert.Equal(bigInt, bigIntFromSequence);
+        }
+    }
+}
--- csharp/Platform.Data.Doublets/Numbers/Raw/BigIntegerToRawNumberSequenceConverter.cs
+++ csharp/Platform.Data.Doublets/Numbers/Raw/BigIntegerToRawNumberSequenceConverter.cs
@@ -1,16 +1,26 @@
 using System.Numerics;
 using Platform.Converters;
 using Platform.Data.Doublets.Decorators;
+using Platform.Unsafe;
+
 
 namespace Platform.Data.Doublets.Numbers.Raw
 {
     public class BigIntegerToRawNumberSequenceConverter<TLink> : LinksDecoratorBase<TLink>, IConverter<BigInteger, TLink>
+    where TLink : struct
     {
-        public BigIntegerToRawNumberSequenceConverter(ILinks<TLink> links) : base(links) { }
+        private readonly IConverter<TLink> _addressToNumberConverter;
+        public readonly int TLinkSize = Structure<TLink>.Size;
+
+        public BigIntegerToRawNumberSequenceConverter(ILinks<TLink> links, IConverter<TLink> addressToNumberConverter) : base(links)
+        {
+            _addressToNumberConverter = addressToNumberConverter;   
+        }
 
         public TLink Convert(BigInteger bigInt)
         {
-            
+            var bigIntAsTLink = ByteArrayExtensions.ToStructure<TLink>(bigInt.ToByteArray());
+            return _addressToNumberConverter.Convert(bigIntAsTLink);
         }
     }
 }
