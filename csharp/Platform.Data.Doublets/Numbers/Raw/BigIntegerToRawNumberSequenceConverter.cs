6e60ec5404ddefafbb1afd3e7bf50ca82c3f3442
FreePhoenix <freephoenix888@gmail.com>
2021-07-31 17:53:21 +0600

    Update draft

--- csharp/Platform.Data.Doublets/Numbers/Raw/BigIntegerToRawNumberSequenceConverter.cs
+++ csharp/Platform.Data.Doublets/Numbers/Raw/BigIntegerToRawNumberSequenceConverter.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Numerics;
 using Platform.Converters;
 using Platform.Data.Doublets.Decorators;
+using Platform.Numbers;
+using Platform.Reflection;
 using Platform.Unsafe;
 
 
@@ -14,6 +16,9 @@ namespace Platform.Data.Doublets.Numbers.Raw
     {
         private readonly IConverter<TLink> _addressToNumberConverter;
         public static readonly int BitsStorableInRawNumber = Structure<TLink>.Size - 1;
+        private static readonly int _bitsPerRawNumber = NumericType<TLink>.BitsSize - 1;
+        private static readonly BigInteger _maximumValue = NumericType<BigInteger>.MaxValue;
+        private static readonly BigInteger _bitMask = Bit.ShiftRight(_maximumValue, 1);
         
         public BigIntegerToRawNumberSequenceConverter(ILinks<TLink> links, IConverter<TLink> addressToNumberConverter) : base(links)
         {
@@ -23,21 +28,21 @@ namespace Platform.Data.Doublets.Numbers.Raw
         public TLink Convert(BigInteger bigInt)
         {
             var currentBigInt = bigInt;
+            var bigIntPart = Bit.And(currentBigInt, _bitMask);
+            var bigIntBytes = bigIntPart.ToByteArray();
+            var nextBigInt = currentBigInt >> 63;
             TLink bigIntLink;
-                var bigIntBytes = currentBigInt.ToByteArray();
-                var next63Bits = currentBigInt >> 63;
-                TLink next63BitsLink;
-                if (next63Bits != 0)
-                {
-                    next63BitsLink = Convert(next63Bits);
-                    var currentBigIntLink = _addressToNumberConverter.Convert(bigIntBytes.ToStructure<TLink>());
-                    bigIntLink = _links.GetOrCreate(currentBigIntLink, next63BitsLink);
-                }
-                else
-                {
-                    bigIntLink = _addressToNumberConverter.Convert(bigIntBytes.ToStructure<TLink>());
-                }
-                return bigIntLink;
+            if (nextBigInt > 0)
+            {
+                TLink nextBigIntLink = Convert(nextBigInt);
+                var currentBigIntLink = _addressToNumberConverter.Convert(bigIntBytes.ToStructure<TLink>());
+                bigIntLink = _links.GetOrCreate(currentBigIntLink, nextBigIntLink);
+            }
+            else
+            {
+                bigIntLink = _addressToNumberConverter.Convert(bigIntBytes.ToStructure<TLink>());
+            }
+            return bigIntLink;
         }
     }
 }
