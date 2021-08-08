bfd5a39cfd7a5284fbc43e35394bacd6d0b12aa2
FreePhoenix <freephoenix888@gmail.com>
2021-08-08 18:33:20 +0600

    Update rational numbers converters

--- csharp/Platform.Data.Doublets/Numbers/Rational/DecimalToRationalConverter.cs
+++ csharp/Platform.Data.Doublets/Numbers/Rational/DecimalToRationalConverter.cs
@@ -27,55 +27,26 @@ namespace Platform.Data.Doublets.Numbers.Rational
         public TLink Convert(decimal @decimal)
         {
             var decimalAsString = @decimal.ToString();
-            int dotPosition = decimalAsString.IndexOf('.');
-            string decimalWithoutDots = decimalAsString;
-            if (dotPosition == -1)
-            {
-                dotPosition = 0;
-            }
-            else
+            var dotPosition = decimalAsString.IndexOf('.');
+            var decimalWithoutDots = decimalAsString;
+            int digitsAfterDot = 0;
+            if (dotPosition != -1)
             {
                 decimalWithoutDots = decimalWithoutDots.Remove(dotPosition, 1);
+                digitsAfterDot = decimalAsString.Length - 1 - dotPosition;
             }
-            BigInteger numerator = BigInteger.Parse(decimalWithoutDots);
-            var numeratorLink = BigIntegerToRawNumberSequenceConverter.Convert(numerator);
-            var dotPositionLink = BigIntegerToRawNumberSequenceConverter.AddressToNumberConverter.Convert(UncheckedConverter.Convert(dotPosition));
-            return _links.GetOrCreate(numeratorLink, dotPositionLink);
-        }
-
-        /* public TLink Convert(decimal @decimal)
-        {
-            var decimalAsString = @decimal.ToString();
-            var isDigitAfterDot = false;
-            int digitsAfterDot = 0;
-            StringBuilder stringBuilder = new(decimalAsString.Length);
-            foreach (var @char in decimalAsString)
-            {
-                if (@char == '.')
-                {
-                    isDigitAfterDot = true;
-                    continue;
-                }
-                if (isDigitAfterDot)
-                {
-                    digitsAfterDot++;
-                }
-                stringBuilder.Append(@char);
-            }
-            var decimalWithoutDots = stringBuilder.ToString();
             BigInteger denominator = new(System.Math.Pow(10, digitsAfterDot));
             BigInteger numerator = BigInteger.Parse(decimalWithoutDots);
-            BigInteger greatestCommonDivisor;
-            do
+            BigInteger greatestCommonDivisor = new(0);
+            while (greatestCommonDivisor != 1)
             {
                 greatestCommonDivisor = BigInteger.GreatestCommonDivisor(numerator, denominator);
                 numerator /= greatestCommonDivisor;
                 denominator /= greatestCommonDivisor;
             }
-            while (greatestCommonDivisor != 1);
             var numeratorLink = BigIntegerToRawNumberSequenceConverter.Convert(numerator);
             var denominatorLink = BigIntegerToRawNumberSequenceConverter.Convert(denominator);
             return _links.GetOrCreate(numeratorLink, denominatorLink);
-        } */
+        }
     }
 }
--- csharp/Platform.Data.Doublets/Numbers/Rational/RationalToDecimalConverter.cs
+++ csharp/Platform.Data.Doublets/Numbers/Rational/RationalToDecimalConverter.cs
@@ -26,16 +26,9 @@ namespace Platform.Data.Doublets.Numbers.Rational
 
         public decimal Convert(TLink rationalNumber)
         {
-            var numerator = RawNumberSequenceToBigIntegerConverter.Convert(_links.GetSource(rationalNumber));
-            var dotPositionLink = UncheckedConverter.Convert(RawNumberSequenceToBigIntegerConverter.NumberToAddressConverter.Convert(_links.GetTarget(rationalNumber)));
-            return dotPositionLink == 0 ? (decimal)numerator : decimal.Parse(numerator.ToString().Insert(dotPositionLink, "."));
+            var numerator = (decimal)RawNumberSequenceToBigIntegerConverter.Convert(_links.GetSource(rationalNumber));
+            var denominator = (decimal)RawNumberSequenceToBigIntegerConverter.Convert(_links.GetTarget(rationalNumber));
+            return numerator / denominator;
         }
-
-        /* public decimal Convert(TLink rationalNumber)
-        {
-            var numerator = RawNumberSequenceToBigIntegerConverter.Convert(_links.GetSource(rationalNumber));
-            var denominator = RawNumberSequenceToBigIntegerConverter.Convert(_links.GetTarget(rationalNumber));
-            return (decimal)(numerator / denominator);
-        } */
     }
 }
