508272c32cc35959a61bad857e5feecc00e71f55
FreePhoenix <freephoenix888@gmail.com>
2021-08-14 22:20:11 +0600

    Add UInt64LinksExtensions.cs

---
+++ csharp/Platform.Data.Doublets/UInt64LinksExtensions.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Platform.Singletons;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Doublets
+{
+    public static class UInt64LinksExtensions
+    {
+        public static readonly LinksConstants<ulong> Constants = Default<LinksConstants<ulong>>.Instance;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool AnyLinkIsAny(this ILinks<ulong> links, params ulong[] sequence)
+        {
+            if (sequence == null)
+            {
+                return false;
+            }
+            var constants = links.Constants;
+            for (var i = 0; i < sequence.Length; i++)
+            {
+                if (sequence[i] == constants.Any)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatStructure(this ILinks<ulong> links, ulong linkIndex, Func<Link<ulong>, bool> isElement, bool renderIndex = false, bool renderDebug = false)
+        {
+            var sb = new StringBuilder();
+            var visited = new HashSet<ulong>();
+            links.AppendStructure(sb, visited, linkIndex, isElement, (innerSb, link) => innerSb.Append(link.Index), renderIndex, renderDebug);
+            return sb.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatStructure(this ILinks<ulong> links, ulong linkIndex, Func<Link<ulong>, bool> isElement, Action<StringBuilder, Link<ulong>> appendElement, bool renderIndex = false, bool renderDebug = false)
+        {
+            var sb = new StringBuilder();
+            var visited = new HashSet<ulong>();
+            links.AppendStructure(sb, visited, linkIndex, isElement, appendElement, renderIndex, renderDebug);
+            return sb.ToString();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AppendStructure(this ILinks<ulong> links, StringBuilder sb, HashSet<ulong> visited, ulong linkIndex, Func<Link<ulong>, bool> isElement, Action<StringBuilder, Link<ulong>> appendElement, bool renderIndex = false, bool renderDebug = false)
+        {
+            if (sb == null)
+            {
+                throw new ArgumentNullException(nameof(sb));
+            }
+            if (linkIndex == Constants.Null || linkIndex == Constants.Any || linkIndex == Constants.Itself)
+            {
+                return;
+            }
+            if (links.Exists(linkIndex))
+            {
+                if (visited.Add(linkIndex))
+                {
+                    sb.Append('(');
+                    var link = new Link<ulong>(links.GetLink(linkIndex));
+                    if (renderIndex)
+                    {
+                        sb.Append(link.Index);
+                        sb.Append(':');
+                    }
+                    if (link.Source == link.Index)
+                    {
+                        sb.Append(link.Index);
+                    }
+                    else
+                    {
+                        var source = new Link<ulong>(links.GetLink(link.Source));
+                        if (isElement(source))
+                        {
+                            appendElement(sb, source);
+                        }
+                        else
+                        {
+                            links.AppendStructure(sb, visited, source.Index, isElement, appendElement, renderIndex);
+                        }
+                    }
+                    sb.Append(' ');
+                    if (link.Target == link.Index)
+                    {
+                        sb.Append(link.Index);
+                    }
+                    else
+                    {
+                        var target = new Link<ulong>(links.GetLink(link.Target));
+                        if (isElement(target))
+                        {
+                            appendElement(sb, target);
+                        }
+                        else
+                        {
+                            links.AppendStructure(sb, visited, target.Index, isElement, appendElement, renderIndex);
+                        }
+                    }
+                    sb.Append(')');
+                }
+                else
+                {
+                    if (renderDebug)
+                    {
+                        sb.Append('*');
+                    }
+                    sb.Append(linkIndex);
+                }
+            }
+            else
+            {
+                if (renderDebug)
+                {
+                    sb.Append('~');
+                }
+                sb.Append(linkIndex);
+            }
+        }
+    }
+}
