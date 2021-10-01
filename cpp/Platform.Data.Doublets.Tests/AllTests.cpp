06114a6999a0007b92032cbdf8bdef203965b726
uselessgoddess <dodickgod@gmail.com>
2021-10-01 15:13:27 +0300

    Translate Doublets::Link

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -4,7 +4,7 @@
 using namespace Platform::Memory;
 using namespace Platform::Data::Doublets;
 
-auto main() -> int {
+auto _0main() -> int {
     using link_type = std::size_t;
     using doublet = Doublet<link_type>;
     auto file = FileMappedResizableDirectMemory("db.links");
@@ -19,4 +19,17 @@ auto main() -> int {
     std::cout << (doublet)mem[1] << std::endl;
     std::cout << (doublet)mem[2] << std::endl;
     std::cout << (doublet)mem[3] << std::endl;
+}
+
+
+auto main() -> int {
+    auto link = Link(1, 2, 3);
+
+    std::cout << Link<int>::ToString(1, 2) << std::endl;
+    std::cout << Link<int>::ToString(1, 2, 3) << std::endl;
+    std::cout << link << std::endl;
+    for (auto item : link) {
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
 }
---
+++ cpp/Platform.Data.Doublets/Link.h
@@ -0,0 +1,90 @@
+﻿namespace Platform::Data::Doublets
+{
+    template<typename TLink>
+    struct Link
+    {
+        public: static constexpr Link Null{};
+
+        private: static constexpr LinksConstants<TLink> _constants{};
+
+        private: static constexpr std::size_t Length = 3;
+
+        public: TLink Index{};
+        public: TLink Source{};
+        public: TLink Target{};
+
+        public: Link() noexcept = default;
+        public: Link(const Link&) noexcept = default;
+        public: Link(Link&&) noexcept = default;
+
+        public: bool operator==(const Link&) const noexcept = default;
+
+        public: Link(TLink index, TLink source, TLink target) : Index(index), Source(source), Target(target) {}
+
+        public: Link(std::ranges::range auto&& range)
+        {
+            for (std::size_t i = 0; auto&& item : range | std::views::take(3))
+            {
+                (*this)[i++] = item; // TODO: later later use std::forward
+            }
+        }
+
+        public: bool IsNull() const noexcept
+        {
+            constexpr auto null = _constants.Null;
+            return *this == Link{null, null, null};
+        }
+
+        public: auto&& operator[](std::size_t index) const
+        {
+            using namespace Platform::Ranges; // TODO: EnsureExtensions
+            Always::ArgumentInRange(index, Range{0, Length - 1}, "index");
+            switch (index)
+            {
+                case _constants.IndexPart:
+                    return Index;
+                case _constants.SourcePart:
+                    return Source;
+                case _constants.TargetPart:
+                    return Target;
+                default:
+                    throw std::logic_error("Not supported exception.");
+            }
+        }
+
+
+        public: std::size_t size() const noexcept
+        {
+            return Length;
+        }
+
+        // TODO: a little unsafe
+        public: auto begin() const noexcept { return &Index; }
+
+        public: auto end() const noexcept { return &Target + 1; }
+
+        public: explicit operator std::string() const { return Index == _constants.Null ? ToString(Source, Target) : ToString(Index, Source, Target); }
+
+        public: friend auto& operator<<(std::ostream& stream, const Link<TLink>& self) { return stream << static_cast<std::string>(self); }
+
+        public: static std::string ToString(TLink index, TLink source, TLink target) { return std::string("(").append(Platform::Converters::To<std::string>(index)).append(": ").append(Platform::Converters::To<std::string>(source)).append("->").append(Platform::Converters::To<std::string>(target)).append(1, ')'); }
+
+        public: static std::string ToString(TLink source, TLink target) { return std::string("(").append(Platform::Converters::To<std::string>(source)).append("->").append(Platform::Converters::To<std::string>(target)).append(1, ')'); }
+    };
+
+    template<typename... Args>
+    Link(Args...) -> Link<std::common_type_t<Args...>>;
+
+    template<std::ranges::range Range>
+    Link(Range) -> Link<std::ranges::range_value_t<Range>>;
+}
+
+template<typename TLink>
+struct std::hash<Platform::Data::Doublets::Link<TLink>>
+{
+    using Self = Platform::Data::Doublets::Link<TLink>;
+    std::size_t operator()(const Self& self) const noexcept
+    {
+        return Platform::Hashing::Hash(self.Index, self.Source, self.Target);
+    }
+};
