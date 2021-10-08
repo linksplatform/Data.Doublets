40f00eaa16f9d29bb70da60024752fa9fdfedc62
uselessgoddess <dodickgod@gmail.com>
2021-10-08 18:07:47 +0300

    [revert]

---
+++ cpp/Platform.Data.Doublets/Decorators/NonNullContentsLinkDeletionResolver.h
@@ -0,0 +1,16 @@
+namespace Platform::Data::Doublets::Decorators
+{
+    template <typename ...> class NonNullContentsLinkDeletionResolver;
+    template <typename TLink> class NonNullContentsLinkDeletionResolver<TLink> : public LinksDecoratorBase<TLink>
+    {
+        public: NonNullContentsLinkDeletionResolver(ILinks<TLink> &links) : LinksDecoratorBase(links) { }
+
+        public: void Delete(IList<TLink> &restrictions) override
+        {
+            auto linkIndex = restrictions[_constants.IndexPart];
+            auto links = _links;
+            links.EnforceResetValues(linkIndex);
+            links.Delete(linkIndex);
+        }
+    };
+}
