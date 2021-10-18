0e46659265a2bf7b02ab6175af90030d718db7f7
uselessgoddess <dodickgod@gmail.com>
2021-10-18 15:43:03 +0300

    Add `Doublet` struct

--- rust/src/doublets/mod.rs
+++ rust/src/doublets/mod.rs
@@ -3,8 +3,10 @@ pub mod mem;
 mod ilinks;
 mod link;
 pub mod decorators;
+mod doublet;
 
 pub use ilinks::ILinks;
 pub use ilinks::ILinksExtensions;
 
 pub use link::Link;
+pub use doublet::Doublet;
