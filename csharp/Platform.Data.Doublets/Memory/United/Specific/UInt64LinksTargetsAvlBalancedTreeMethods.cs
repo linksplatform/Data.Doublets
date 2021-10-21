b13a292b149bed8c91b3453cd6368ee78e30176d
uselessgoddess <dodickgod@gmail.com>
2021-10-21 16:06:24 +0300

    Ugh merge

---
+++ csharp/Platform.Data.Doublets/Memory/United/Specific/UInt64LinksTargetsAvlBalancedTreeMethods.cs
@@ -0,0 +1,383 @@
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Data.Doublets.Memory.United.Specific
+{
+    /// <summary>
+    /// <para>
+    /// Represents the int 64 links targets avl balanced tree methods.
+    /// </para>
+    /// <para></para>
+    /// </summary>
+    /// <seealso cref="UInt64LinksAvlBalancedTreeMethodsBase"/>
+    public unsafe class UInt64LinksTargetsAvlBalancedTreeMethods : UInt64LinksAvlBalancedTreeMethodsBase
+    {
+        /// <summary>
+        /// <para>
+        /// Initializes a new <see cref="UInt64LinksTargetsAvlBalancedTreeMethods"/> instance.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="constants">
+        /// <para>A constants.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="links">
+        /// <para>A links.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="header">
+        /// <para>A header.</para>
+        /// <para></para>
+        /// </param>
+        public UInt64LinksTargetsAvlBalancedTreeMethods(LinksConstants<ulong> constants, RawLink<ulong>* links, LinksHeader<ulong>* header) : base(constants, links, header) { }
+
+        /// <summary>
+        /// <para>
+        /// Gets the left reference using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ref ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ref ulong GetLeftReference(ulong node) => ref Links[node].LeftAsTarget;
+
+        /// <summary>
+        /// <para>
+        /// Gets the right reference using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ref ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ref ulong GetRightReference(ulong node) => ref Links[node].RightAsTarget;
+
+        /// <summary>
+        /// <para>
+        /// Gets the left using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ulong GetLeft(ulong node) => Links[node].LeftAsTarget;
+
+        /// <summary>
+        /// <para>
+        /// Gets the right using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ulong GetRight(ulong node) => Links[node].RightAsTarget;
+
+        /// <summary>
+        /// <para>
+        /// Sets the left using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="left">
+        /// <para>The left.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetLeft(ulong node, ulong left) => Links[node].LeftAsTarget = left;
+
+        /// <summary>
+        /// <para>
+        /// Sets the right using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="right">
+        /// <para>The right.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetRight(ulong node, ulong right) => Links[node].RightAsTarget = right;
+
+        /// <summary>
+        /// <para>
+        /// Gets the size using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ulong GetSize(ulong node) => GetSizeValue(Links[node].SizeAsTarget);
+
+        /// <summary>
+        /// <para>
+        /// Sets the size using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="size">
+        /// <para>The size.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetSize(ulong node, ulong size) => SetSizeValue(ref Links[node].SizeAsTarget, size);
+
+        /// <summary>
+        /// <para>
+        /// Determines whether this instance get left is child.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The bool</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override bool GetLeftIsChild(ulong node) => GetLeftIsChildValue(Links[node].SizeAsTarget);
+
+        /// <summary>
+        /// <para>
+        /// Sets the left is child using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="value">
+        /// <para>The value.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetLeftIsChild(ulong node, bool value) => SetLeftIsChildValue(ref Links[node].SizeAsTarget, value);
+
+        /// <summary>
+        /// <para>
+        /// Determines whether this instance get right is child.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The bool</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override bool GetRightIsChild(ulong node) => GetRightIsChildValue(Links[node].SizeAsTarget);
+
+        /// <summary>
+        /// <para>
+        /// Sets the right is child using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="value">
+        /// <para>The value.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetRightIsChild(ulong node, bool value) => SetRightIsChildValue(ref Links[node].SizeAsTarget, value);
+
+        /// <summary>
+        /// <para>
+        /// Gets the balance using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The sbyte</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override sbyte GetBalance(ulong node) => GetBalanceValue(Links[node].SizeAsTarget);
+
+        /// <summary>
+        /// <para>
+        /// Sets the balance using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="value">
+        /// <para>The value.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void SetBalance(ulong node, sbyte value) => SetBalanceValue(ref Links[node].SizeAsTarget, value);
+
+        /// <summary>
+        /// <para>
+        /// Gets the tree root.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <returns>
+        /// <para>The ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ulong GetTreeRoot() => Header->RootAsTarget;
+
+        /// <summary>
+        /// <para>
+        /// Gets the base part value using the specified link.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="link">
+        /// <para>The link.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The ulong</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override ulong GetBasePartValue(ulong link) => Links[link].Target;
+
+        /// <summary>
+        /// <para>
+        /// Determines whether this instance first is to the left of second.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="firstSource">
+        /// <para>The first source.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="firstTarget">
+        /// <para>The first target.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="secondSource">
+        /// <para>The second source.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="secondTarget">
+        /// <para>The second target.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The bool</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override bool FirstIsToTheLeftOfSecond(ulong firstSource, ulong firstTarget, ulong secondSource, ulong secondTarget)
+            => firstTarget < secondTarget || (firstTarget == secondTarget && firstSource < secondSource);
+
+        /// <summary>
+        /// <para>
+        /// Determines whether this instance first is to the right of second.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="firstSource">
+        /// <para>The first source.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="firstTarget">
+        /// <para>The first target.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="secondSource">
+        /// <para>The second source.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="secondTarget">
+        /// <para>The second target.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The bool</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override bool FirstIsToTheRightOfSecond(ulong firstSource, ulong firstTarget, ulong secondSource, ulong secondTarget)
+            => firstTarget > secondTarget || (firstTarget == secondTarget && firstSource > secondSource);
+
+        /// <summary>
+        /// <para>
+        /// Clears the node using the specified node.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="node">
+        /// <para>The node.</para>
+        /// <para></para>
+        /// </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void ClearNode(ulong node)
+        {
+            ref var link = ref Links[node];
+            link.LeftAsTarget = 0UL;
+            link.RightAsTarget = 0UL;
+            link.SizeAsTarget = 0UL;
+        }
+    }
+}
