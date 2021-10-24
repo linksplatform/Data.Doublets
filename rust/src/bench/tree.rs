dbf4fbb7c15cdb3864bf0ee545addfcf8622628e
uselessgoddess <dodickgod@gmail.com>
2021-10-24 12:58:14 +0300

    Create hard tree benchmark

---
+++ rust/src/bench/tree.rs
@@ -0,0 +1,19 @@
+extern crate test;
+
+use crate::methods::NonRecurTree;
+use crate::methods::RecursionlessSizeBalancedTreeMethods;
+use crate::methods::SizeBalancedTreeBase;
+use crate::methods::SizeBalancedTreeMethods;
+use test::Bencher;
+
+#[bench]
+fn non_recur_tree_hard(b: &mut Bencher) {
+    let mut tree = NonRecurTree::new(100000001);
+    let root = &mut tree.root as *mut usize;
+
+    let mut last = 1;
+    b.iter(|| {
+        tree.attach(unsafe { &mut *root }, last);
+        last += 1;
+    });
+}
