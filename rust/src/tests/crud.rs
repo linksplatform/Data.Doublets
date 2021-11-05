981363c23d96a67a217b8e25c641c7643239860b
uselessgoddess <dodickgod@gmail.com>
2021-11-05 19:03:36 +0300

    Add test to compare with postgres

--- rust/src/tests/crud.rs
+++ rust/src/tests/crud.rs
@@ -84,3 +84,22 @@ fn billion_points_bump_alloc() {
 
     println!("{:?}", instant.elapsed());
 }
+
+#[test]
+fn many_points_and_searches() {
+    let bump = Bump::new();
+    let mem = AllocMem::new(&bump);
+    let mut links = make_links(mem);
+
+    let instant = Instant::now();
+    for _ in 0..10_000_000 {
+        links.create_point();
+    }
+    println!("{:?}", instant.elapsed());
+
+    let instant = Instant::now();
+    for i in 0..10_000_000 {
+        links.search_or(i, i, 0);
+    }
+    println!("{:?}", instant.elapsed());
+}
