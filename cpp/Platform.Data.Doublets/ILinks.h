4c0f9538a55e8c6876ba448250fd543fe22eb592
uselessgoddess <dodickgod@gmail.com>
2021-11-12 16:59:00 +0300

    Clear `binaries` branch

--- cpp/Platform.Data.Doublets/ILinks.h
+++
@@ -1,8 +0,0 @@
-﻿namespace Platform::Data::Doublets
-{
-    template <typename ...> class ILinks;
-    template <typename TLink> class ILinks<TLink> : public ILinks<TLink, LinksConstants<TLink>>
-    {
-    public:
-    };
-}
--- cpp/Platform.Data.Doublets/LinkExtensions.h
+++
@@ -1,9 +0,0 @@
-﻿namespace Platform::Data::Doublets
-{
-    class LinkExtensions
-    {
-        public: template <typename TLink> static bool IsFullPoint(Link<TLink> link) { return Point<TLink>.IsFullPoint(link); }
-
-        public: template <typename TLink> static bool IsPartialPoint(Link<TLink> link) { return Point<TLink>.IsPartialPoint(link); }
-    };
-}
