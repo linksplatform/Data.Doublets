1e67461a043a3e8dbaaa20fa341879aaaf54c03b
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 14:51:14 +0600

    Remove XML Documentation for tests and benchmarks

--- csharp/Platform.Data.Doublets.Benchmarks/Program.cs
+++ csharp/Platform.Data.Doublets.Benchmarks/Program.cs
@@ -4,12 +4,6 @@ namespace Platform.Data.Doublets.Benchmarks
 {
     class Program
     {
-        /// <summary>
-        /// <para>
-        /// Main.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         static void Main()
         {
             BenchmarkRunner.Run<MemoryBenchmarks>();
