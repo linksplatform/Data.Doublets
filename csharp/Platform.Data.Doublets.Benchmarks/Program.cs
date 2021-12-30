e1b9de5e73f872265ebaf09a71434119f2e52ff8
FreePhoenix888 <freephoenix888@gmail.com>
2021-12-30 18:19:55 +0600

    Revert "Add LinkStructBenchmarks"
    This reverts commit 68dcb3f5f49986df2f95e8c079beba4cd0c1b28f.

--- csharp/Platform.Data.Doublets.Benchmarks/Program.cs
+++ csharp/Platform.Data.Doublets.Benchmarks/Program.cs
@@ -6,8 +6,7 @@ namespace Platform.Data.Doublets.Benchmarks
     {
         static void Main()
         {
-            //BenchmarkRunner.Run<MemoryBenchmarks>();
-            BenchmarkRunner.Run<LinkStructBenchmarks>();
+            BenchmarkRunner.Run<MemoryBenchmarks>();
         }
     }
 }
