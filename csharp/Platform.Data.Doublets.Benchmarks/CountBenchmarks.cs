02b88e87af22f17b8cb6b3a57af6668e55055c57
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-02 22:47:54 +0600

    Add `CountBenchmarks`

---
+++ csharp/Platform.Data.Doublets.Benchmarks/CountBenchmarks.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Platform.Collections.Arrays;
+using Platform.Collections.Lists;
+using Platform.Converters;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Memory;
+using TLink = System.UInt64;
+
+#pragma warning disable CA1822 // Mark members as static
+
+namespace Platform.Data.Doublets.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class CountBenchmarks
+    {
+        private static ILinks<TLink> _links;
+        private static TLink _any;
+        private static CheckedConverter<TLink, long> _addressToInt64Converter = CheckedConverter<TLink, long>.Default;
+        [GlobalSetup]
+        public static void Setup()
+        {
+            var dataMemory = new HeapResizableDirectMemory();
+            _links = new UnitedMemoryLinks<TLink>(dataMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
+            _any = _links.Constants.Any;
+            _links.CreatePoint();
+        }
+
+        [GlobalCleanup]
+        public static void Cleanup() { }
+
+        [Benchmark]
+        public IList<IList<TLink>> Array()
+        {
+            var addressToInt64Converter = CheckedConverter<TLink, long>.Default;
+            var usagesAsSourceQuery = new Link<TLink>(_any, 1UL, _any);
+            var usagesAsSourceCount = addressToInt64Converter.Convert(_links.Count(usagesAsSourceQuery));
+            var usagesAsTargetQuery = new Link<TLink>(_any, _any, 1UL);
+            var usagesAsTargetCount = addressToInt64Converter.Convert(_links.Count(usagesAsTargetQuery));
+            var totalUsages = usagesAsSourceCount + usagesAsTargetCount;
+            var usages = new IList<TLink>[totalUsages];
+            var usagesFiller = new ArrayFiller<IList<TLink>, TLink>(usages, _links.Constants.Continue);
+            _links.Each(usagesFiller.AddAndReturnConstant, usagesAsSourceQuery);
+            _links.Each(usagesFiller.AddAndReturnConstant, usagesAsTargetQuery);
+            return usages;
+        }
+        [Benchmark]
+        public IList<IList<TLink>> List()
+        {
+            var usagesAsSourceQuery = new Link<TLink>(_any, 1UL, _any);
+            var usagesAsTargetQuery = new Link<TLink>(_any, _any, 1UL);
+            var usages = new List<IList<TLink>>();
+            var usagesFiller = new ListFiller<IList<TLink>, TLink>(usages, _links.Constants.Continue);
+            _links.Each(usagesFiller.AddAndReturnConstant, usagesAsSourceQuery);
+            _links.Each(usagesFiller.AddAndReturnConstant, usagesAsTargetQuery);
+            return usages;
+        }
+    }
+}
