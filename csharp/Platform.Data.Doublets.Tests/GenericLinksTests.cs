0efd040b7ef26dc3f4eef8bf620bb9de4d98006f
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-18 10:47:02 +0600

    Use `LoggingDecorator`

--- csharp/Platform.Data.Doublets.Tests/GenericLinksTests.cs
+++ csharp/Platform.Data.Doublets.Tests/GenericLinksTests.cs
@@ -1,9 +1,10 @@
 using System;
 using System.IO;
+using Platform.Data.Doublets.Decorators;
 using Xunit;
-using Platform.Reflection;
+
 using Platform.Memory;
-using Platform.Scopes;
+
 using Platform.Data.Doublets.Memory.United.Generic;
 
 namespace Platform.Data.Doublets.Tests
@@ -38,14 +39,14 @@ namespace Platform.Data.Doublets.Tests
         }
         private static void Using<TLink>(Action<ILinks<TLink>> action)
         {
-            using (var scope = new Scope<Types<HeapResizableDirectMemory, UnitedMemoryLinks<TLink>>>())
-            {
-                action(scope.Use<ILinks<TLink>>());
-            }
-            File.Delete("db.links");
-
-            using var links = new FFI.UnitedMemoryLinks<TLink>("db.links");
+            var unitedMemoryLinks = new UnitedMemoryLinks<TLink>(new HeapResizableDirectMemory());
+            var logFile = File.Open("linksLogger.txt", FileMode.Create, FileAccess.Write);
+            LoggingDecorator<TLink> links = new(unitedMemoryLinks, logFile);
             action(links);
+
+            File.Delete("db.links");
+            using var ffiLinks = new FFI.UnitedMemoryLinks<TLink>("db.links");
+            action(ffiLinks);
         }
     }
 }
