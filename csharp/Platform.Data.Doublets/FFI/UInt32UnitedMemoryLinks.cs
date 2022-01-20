8182316c168f61f8f0ce6f4bdbbffb8a69449eee
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-20 13:29:50 +0600

    Add `UInt32UnitedMemoryLinks`

---
+++ csharp/Platform.Data.Doublets/FFI/UInt32UnitedMemoryLinks.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Platform.Converters;
+using Platform.Delegates;
+using Platform.Disposables;
+
+namespace Platform.Data.Doublets.FFI
+{
+    using TLink = System.UInt32;
+
+    public class UInt32UnitedMemoryLinks : DisposableBase, ILinks<TLink>
+    {
+        public LinksConstants<TLink> Constants { get; }
+
+        private readonly unsafe void* _ptr;
+
+        public UInt32UnitedMemoryLinks(string path)
+        {
+            unsafe
+            {
+                _ptr = Methods.UInt32UnitedMemoryLinks_New(path);
+
+                // TODO: Update api
+                Constants = new LinksConstants<TLink>(enableExternalReferencesSupport: true);
+            }
+        }
+
+        public TLink Count(IList<TLink> restriction)
+        {
+            unsafe
+            {
+                var array = stackalloc uint[restriction.Count];
+                for (var i = 0; i < restriction.Count; i++)
+                {
+                    array[i] = restriction[i];
+                }
+                return Methods.UInt32UnitedMemoryLinks_Count(_ptr, array, (nuint)(restriction?.Count ?? 0));
+            }
+        }
+
+        public TLink Each(IList<TLink> restriction, ReadHandler<TLink> handler)
+        {
+            unsafe
+            {
+                Methods.EachCallback_UInt32 callback = (link) => handler?.Invoke(new Link<TLink>(link.Index, link.Source, link.Target)) ?? Constants.Continue;
+                var array = stackalloc uint[restriction.Count];
+                for (var i = 0; i < restriction.Count; i++)
+                {
+                    array[i] = restriction[i];
+                }
+                return Methods.UInt32UnitedMemoryLinks_Each(_ptr, array, (nuint)(restriction?.Count ?? 0), callback);
+            }
+        }
+
+        public TLink Create(IList<TLink> substitution, WriteHandler<TLink> handler)
+        {
+            unsafe
+            {
+                Methods.CreateCallback_UInt32 callback = (before, after) => handler != null ? handler(new Link<TLink>(before.Index, before.Source, before.Target), new Link<TLink>(after.Index, after.Source, after.Target)) : Constants.Continue;
+                fixed (uint* substitutionPtr = (uint[])substitution)
+                {
+                    return Methods.UInt32UnitedMemoryLinks_Create(_ptr, substitutionPtr, (nuint)(substitution?.Count ?? 0), callback);
+                }
+            }
+        }
+
+        public TLink Update(IList<TLink> restriction, IList<TLink> substitution, WriteHandler<TLink> handler)
+        {
+            unsafe
+            {
+                var restrictionArray = restriction.ToArray();
+                var substitutionArray = substitution.ToArray();
+                Methods.UpdateCallback_UInt32 callback = (before, after) => handler?.Invoke(new Link<TLink>(before.Index, before.Source, before.Target), new Link<TLink>(after.Index, after.Source, after.Target)) ?? Constants.Continue;
+                fixed (uint* restrictionPointer = restrictionArray, substitutionPointer = substitutionArray)
+                {
+                    return Methods.UInt32UnitedMemoryLinks_Update(_ptr, restrictionPointer, (nuint)(restrictionArray?.Length ?? 0), substitutionPointer, (nuint)(substitutionArray?.Length ?? 0), callback);
+                }
+            }
+        }
+
+        public TLink Delete(IList<TLink> restriction, WriteHandler<TLink> handler)
+        {
+            unsafe
+            {
+                var restrictionArray = restriction.ToArray();
+                Methods.DeleteCallback_UInt32 callback = (before, after) => handler?.Invoke(new Link<TLink>(before.Index, before.Source, before.Target), new Link<TLink>(after.Index, after.Source, after.Target)) ?? Constants.Continue;
+                fixed (uint* restrictionPointer = restrictionArray)
+                {
+                    return Methods.UInt32UnitedMemoryLinks_Delete(_ptr, restrictionPointer, (nuint)(restrictionArray?.Length ?? 0), callback);
+                }
+            }
+        }
+
+        protected override void Dispose(bool manual, bool wasDisposed)
+        {
+            unsafe
+            {
+                if (wasDisposed || _ptr == null)
+                {
+                    return;
+                }
+                Methods.UInt32UnitedMemoryLinks_Drop(_ptr);
+            }
+        }
+    }
+}
