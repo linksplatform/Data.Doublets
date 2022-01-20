aecf530680316ffb8cdff641c37805a88672a2cc
FreePhoenix888 <freephoenix888@gmail.com>
2022-01-20 13:31:29 +0600

    Add `SerialCreationsAndDeletionsBenchmark`

---
+++ csharp/Platform.Data.Doublets.Benchmarks/SerialCreationsAndDeletionsBenchmark.cs
@@ -0,0 +1,72 @@
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using Platform.Data.Doublets.Memory.Split.Generic;
+using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Tests;
+using Platform.Memory;
+
+#pragma warning disable CA1822 // Mark members as static
+
+namespace Platform.Data.Doublets.Benchmarks
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class SerialCreationsAndDeletionsBenchmark
+    {
+        private static SplitMemoryLinks<uint> _splitMemory;
+        private static UnitedMemoryLinks<uint> _unitedMemory;
+        private static FFI.UnitedMemoryLinks<uint> _ffiUnitedMemory;
+        private static FFI.UInt32UnitedMemoryLinks _uint32FfiUnitedMemory;
+
+        [GlobalSetup]
+        public static void Setup()
+        {
+            var dataMemory = new HeapResizableDirectMemory();
+            var indexMemory = new HeapResizableDirectMemory();
+            _splitMemory = new SplitMemoryLinks<uint>(dataMemory, indexMemory);
+
+            var memory = new HeapResizableDirectMemory();
+            _unitedMemory = new UnitedMemoryLinks<uint>(memory);
+
+            File.Delete("db.links");
+            _ffiUnitedMemory = new FFI.UnitedMemoryLinks<uint>("db.links");
+
+            File.Delete("db.links");
+            _uint32FfiUnitedMemory = new FFI.UInt32UnitedMemoryLinks("db.links");
+        }
+
+        [GlobalCleanup]
+        public static void Cleanup()
+        {
+            _splitMemory.Dispose();
+            _unitedMemory.Dispose();
+             _ffiUnitedMemory.Dispose();
+             _uint32FfiUnitedMemory.Dispose();
+            File.Delete("db.links");
+        }
+
+        [Benchmark]
+        public void Split()
+        {
+            _splitMemory.TestMultipleCreationsAndDeletions(10000);
+        }
+
+        [Benchmark]
+        public void United()
+        {
+            _unitedMemory.TestMultipleCreationsAndDeletions(10000);
+        }
+
+        [Benchmark]
+        public void FfiUnited()
+        {
+            _ffiUnitedMemory.TestMultipleCreationsAndDeletions(10000);
+        }
+
+        [Benchmark]
+        public void UInt32FfiUnited()
+        {
+            _uint32FfiUnitedMemory.TestMultipleCreationsAndDeletions(10000);
+        }
+    }
+}
