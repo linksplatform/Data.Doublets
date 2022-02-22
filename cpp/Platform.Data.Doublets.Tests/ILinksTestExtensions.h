1d00b15cb5ef80c3746d224d98b0932acf51dda6
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-22 22:50:37 +0600

    Update `ILinksTextExtensions`

--- cpp/Platform.Data.Doublets.Tests/ILinksTestExtensions.h
+++ cpp/Platform.Data.Doublets.Tests/ILinksTestExtensions.h
@@ -1,24 +1,25 @@
 namespace Platform::Data::Doublets::Tests
 {
+    template<typename TSelf>
     class ILinksTestExtensions
     {
     public:
         template<typename TLinkAddress>
-        static void TestCrudOperations(ILinks<TLinkAddress> storage)
+        static void TestCrudOperations(ILinks<TSelf, TLinkAddress>& storage)
         {
             const auto constants = storage.Constants;
             ASSERT_TRUE(0 == storage.Count());
             // Create link
-            Setter<TLinkAddress, TLinkAddress> setter { constants.Continue, constants.Break, constants.Null };
+            Setters::Setter<TLinkAddress, TLinkAddress> setter { constants.Continue, constants.Break, constants.Null };
             storage.Each(Link{constants.Any, constants.Any, constants.Any}, setter.SetFirstFromList);
-            ASSERT_TRUE(constants.Null == setter.Result())
+            ASSERT_TRUE(constants.Null == setter.Result());
             auto linkAddress = storage.Create();
             Link<TLinkAddress> link { storage.GetLink(linkAddress) };
             ASSERT_TRUE(3 == link.Count);
             ASSERT_TRUE(linkAddress == link.Index);
             ASSERT_TRUE(constants.Null == link.Source);
             ASSERT_TRUE(constants.Null == link.Target);
-            ASSERT_TRUE(1 == storage.Count())
+            ASSERT_TRUE(1 == storage.Count());
             // Get first link
             setter = {constants.Continue, constants.Break, constants.Null};
             storage.Each(Link{constants.Any, constants.Any, constants.Any}, setter.SetFirstFromListAndReturnTrue);
@@ -43,20 +44,20 @@ namespace Platform::Data::Doublets::Tests
         }
 
         template<typename TLinkAddress>
-        static void TestRawNumbersCrudOperations(ILinks<TLinkAddress>)
+        static void TestRawNumbersCrudOperations(ILinks<TSelf, TLinkAddress>& storage)
         {
             // Constants
             const auto constants = storage.Constants;
             Hybrid<TLinkAddress> h106E {106L, true};
             Hybrid<TLinkAddress> h107E {107L, true};
-            Hybrid<TLinkAddress> h106E {-108L, true};
+            Hybrid<TLinkAddress> h108E {-108L, true};
             ASSERT_TRUE(106L == h106E.AbsoluteValue());
             ASSERT_TRUE(107L == h107E.AbsoluteValue());
             ASSERT_TRUE(108L == h108E.AbsoluteValue());
             // Create link (External -> External)
             auto linkAddress1 = storage.Create();
             storage.Update(linkAddress1, h106E, h108E);
-            Link<TLinkAddress> link1 { Storage.GetLink(linkAddress1) };
+            Link<TLinkAddress> link1 { storage.GetLink(linkAddress1) };
             ASSERT_TRUE(h106E == link1.Source);
             ASSERT_TRUE(h108E == link1.Target);
             // Create link (Internal -> External)
@@ -72,29 +73,29 @@ namespace Platform::Data::Doublets::Tests
             ASSERT_TRUE(linkAddress1 == link3.Source);
             ASSERT_TRUE(linkAddress1 == link3.Target);
             // Search for created link
-            Setter<TLinkAddress, TLinkAddress> setter1 { constants.Continue, constants.Break, constants.Null };
-            storage.Each(Link<TLinkAddress>{constants.Any, h106E, h108E}, setter.SetFirstFromListAndReturnTrue);
+            Setters::Setter<TLinkAddress, TLinkAddress> setter1 { constants.Continue, constants.Break, constants.Null };
+            storage.Each(Link<TLinkAddress>{constants.Any, h106E, h108E}, setter1.SetFirstFromListAndReturnTrue);
             ASSERT_TRUE(linkAddress1 == setter1.Result);
             // Search for nonexistent link
-            Setter<TLinkAddress, TLinkAddress> setter2 { constants.Continue, constants.Break, constants.Null };
+            Setters::Setter<TLinkAddress, TLinkAddress> setter2 { constants.Continue, constants.Break, constants.Null };
             storage.Each(Link<TLinkAddress>{constants.Any, h106E, h108E}, setter2.SetFirstFromListAndReturnTrue);
             ASSERT_TRUE(constants.Null == setter2.Result);
             // Update link to reference null (prepare for delete)
-            auto updatedLinkAddress { links.Update(linkAddress3, constants.Null, constants.Null) };
+            auto updatedLinkAddress { storage.Update(linkAddress3, constants.Null, constants.Null) };
             ASSERT_TRUE(linkAddress3 == updatedLinkAddress);
-            Link<TLinkAddress> link3 = { links.GetLink(linkAddress3) };
+            link3 = { storage.GetLink(linkAddress3) };
             ASSERT_TRUE(constants.Null == link3.Source);
             ASSERT_TRUE(constants.Null == link3.Target);
             // Delete link
-            links.Delete(linkAddress3);
-            ASSERT_TRUE(two == links.Count());
-            Setter<TLinkAddress, TLinkAddress> setter3 { constants.Continue, constants.Break, constants.Null) };
-            links.Each(Link<TLinkAddress>{ constants.Any, constants.Any, constants.Any }, setter3.SetFirstFromListAndReturnTrue);
+            storage.Delete(linkAddress3);
+            ASSERT_TRUE(2 == storage.Count());
+            Setters::Setter<TLinkAddress, TLinkAddress> setter3 { constants.Continue, constants.Break, constants.Null };
+            storage.Each(Link<TLinkAddress>{ constants.Any, constants.Any, constants.Any }, setter3.SetFirstFromListAndReturnTrue);
             ASSERT_TRUE(linkAddress2 == setter3.Result);
         }
 
         template<typename TLinkAddress>
-        static void TestMultipleCreationsAndDeletions(ILinks<TLinkAddress> storage, int numberOfOperations)
+        static void TestMultipleCreationsAndDeletions(ILinks<TSelf, TLinkAddress>& storage, int numberOfOperations)
         {
             for (int i = 0; i < numberOfOperations; i++)
             {
@@ -107,23 +108,27 @@ namespace Platform::Data::Doublets::Tests
         }
 
         template<typename TLinkAddress>
-        static void TestMultipleRandomCreationsAndDeletions(ILinks<TLinkAddress> storage, int maximumOperationsPerCycle)
+        static void TestMultipleRandomCreationsAndDeletions(ILinks<TSelf, TLinkAddress>& storage, int maximumOperationsPerCycle)
         {
-            for (auto N { 1 }; N < maximumOperationsPerCycle; N++)
+//            using namespace Platform::Random;
+            for (auto N { 1 }; N < maximumOperationsPerCycle; ++N)
             {
-                auto random { std::rand(N) };
+                std::srand(N);
+//                auto randomInt { std::rand() };
+//                auto random { Platform::Random::NextBoolean(std::rand) };
+                std::mt19937_64 randomGen64 {};
                 auto created { 0UL };
                 auto deleted { 0UL };
                 for (auto i { 0 }; i < N; ++i)
                 {
-                    auto linksCount { addressToUInt64Converter.Convert(storage.Count()) };
-                    auto createPoint { random.NextBoolean() };
+                    auto linksCount { storage.Count() };
+                    auto createPoint { Random::NextBoolean(randomGen64) };
                     if (linksCount >= 2 && createPoint)
                     {
-                        Range<TLinkAddress> linksAddressRange { new (1, linksCount) };
-                        TLinkAddress source { random.NextUInt64(linksAddressRange) };
-                        TLinkAddress target { random.NextUInt64(linksAddressRange) }; //-V3086
-                        auto resultLink { links.GetOrCreate(source, target) };
+                        Ranges::Range<TLinkAddress> linksAddressRange { 1, linksCount };
+                        TLinkAddress source { Random::NextUInt64(linksAddressRange) };
+                        TLinkAddress target { Random::NextUInt64(linksAddressRange) }; //-V3086
+                        auto resultLink { storage.GetOrCreate(source, target) };
                         if (resultLink > linksCount)
                         {
                             ++created;
