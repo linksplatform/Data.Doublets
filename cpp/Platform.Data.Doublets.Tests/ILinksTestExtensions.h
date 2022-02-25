38a70c69a903873b05a70d2f23d2c52b36e63af9
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-25 23:54:59 +0600

    Update `ILinksTestExtensions`

--- cpp/Platform.Data.Doublets.Tests/ILinksTestExtensions.h
+++ cpp/Platform.Data.Doublets.Tests/ILinksTestExtensions.h
@@ -1,42 +1,99 @@
 namespace Platform::Data::Doublets::Tests
 {
+    class CrudOperationsTester
+    {
+    public:
+        template<typename TLinkAddress>
+        static TLinkAddress TestCreate(auto&& storage)
+        {
+            using namespace Platform::Interfaces;
+            auto constants { storage.Constants };
+            Link<TLinkAddress> linkStruct {  };
+            storage.Each( std::array{constants.Any, constants.Any, constants.Any} , [&constants, &linkStruct](Interfaces::CArray auto&& link) {
+                linkStruct = link;
+                return constants.Continue;
+            });
+            ASSERT_EQ(constants.Null, linkStruct.Index);
+            auto linkAddress { storage.Create() };
+            linkStruct = { storage.GetLink(linkAddress) };
+            ASSERT_EQ(3, linkStruct.Count);
+            ASSERT_EQ(linkAddress, linkStruct.Index);
+            ASSERT_EQ(constants.Null, linkStruct.Source);
+            ASSERT_EQ(constants.Null, linkStruct.Target);
+            ASSERT_EQ(1, storage.Count());
+            return linkAddress;
+        }
+
+        template<typename TLinkAddress>
+        static TLinkAddress GetFirstLink(auto&& storage, TLinkAddress linkAddress)
+        {
+            using namespace Platform::Interfaces;
+            auto constants { storage.Constants() };
+            TLinkAddress linkAddressFromEach {};
+            storage.Each(Link{constants.Any, constants.Any, constants.Any}, [&constants, &linkAddressFromEach](Interfaces::CArray auto&& link) {
+                linkAddressFromEach = link[constants.IndexPart];
+                return constants.Continue;
+            });
+            ASSERT_EQ(linkAddress, linkAddressFromEach);
+            return linkAddressFromEach;
+        }
+
+        template<typename TLinkAddress>
+        static TLinkAddress TestUpdateLinkToReferenceItself(auto&& storage, TLinkAddress linkAddress)
+        {
+            auto constants { storage.Constants() };
+            auto link { storage.Update(linkAddress, linkAddress, linkAddress) };
+            Link linkStruct { link };
+            ASSERT_EQ(linkStruct.Index, linkStruct.Source);
+            ASSERT_EQ(linkStruct.Index, linkStruct.Target);
+            return linkStruct.Index;
+        }
+
+        template<typename TLinkAddress>
+        static TLinkAddress TestUpdateLinkToReferenceNull(auto&& storage, TLinkAddress linkAddress)
+        {
+            auto constants { storage.Constants() };
+            auto link { storage.Update(linkAddress, linkAddress, linkAddress) };
+            Link linkStruct { link };
+            ASSERT_EQ(constants.Null, linkStruct.Source);
+            ASSERT_EQ(constants.Null, linkStruct.Target);
+            return linkStruct.Index;
+        }
+
+        template<typename TLinkAddress>
+        static void TestDelete(auto&& storage, TLinkAddress linkAddress)
+        {
+            using namespace Platform::Interfaces;
+            auto constants { storage.Constants() };
+            auto link { storage.Update(linkAddress, linkAddress, linkAddress) };
+            Link linkStruct { link };
+            storage.Delete(linkAddress);
+            ASSERT_EQ(0, storage.Count());
+            TLinkAddress deletedLinkAddress {};
+            storage.Each(Link{constants.Any, constants.Any, constants.Any}, [&constants, &deletedLinkAddress](Interfaces::CArray auto&& link) {
+                deletedLinkAddress = link[constants.IndexPart];
+                return constants.Continue;
+            });
+            ASSERT_EQ(constants.Null, deletedLinkAddress);
+        }
+    };
+
+    
     template<typename TLinkAddress>
     static void TestCrudOperations(auto&& storage)
     {
         const auto constants = storage.Constants;
         ASSERT_EQ(0, storage.Count());
         // Create link
-        Setters::Setter<TLinkAddress, TLinkAddress> setter { constants.Continue, constants.Break, constants.Null };
-        storage.Each( std::array{constants.Any, constants.Any, constants.Any} , setter.SetFirstAndReturnTrue);
-        ASSERT_EQ(constants.Null, setter.Result());
-        auto linkAddress = storage.Create();
-        Link<TLinkAddress> link { storage.GetLink(linkAddress) };
-        ASSERT_EQ(3, link.Count);
-        ASSERT_EQ(linkAddress, link.Index);
-        ASSERT_EQ(constants.Null, link.Source);
-        ASSERT_EQ(constants.Null, link.Target);
-        ASSERT_EQ(1, storage.Count());
+        auto linkAddress { CrudOperationsTester::TestCreate<TLinkAddress>(storage) };
         // Get first link
-        setter = {constants.Continue, constants.Break, constants.Null};
-        storage.Each(Link{constants.Any, constants.Any, constants.Any}, setter.SetFirstFromListAndReturnTrue);
-        ASSERT_EQ(linkAddress, setter.Result());
+        linkAddress = { CrudOperationsTester::GetFirstLink<TLinkAddress>(storage, linkAddress) };
         // Update link to reference itself
-        storage.Update(linkAddress, linkAddress, linkAddress);
-        link = Link{storage.GetLink(linkAddress)};
-        ASSERT_EQ(linkAddress, link.Source);
-        ASSERT_EQ(linkAddress, link.Target);
+        linkAddress = { CrudOperationsTester::TestUpdateLinkToReferenceItself(storage, linkAddress) };
         // Update link to reference null (prepare for delete)
-        auto updatedLinkAddress = storage.Update(linkAddress, constants.Null, constants.Null);
-        ASSERT_EQ(linkAddress, updatedLinkAddress);
-        link = {storage.GetLink(linkAddress)};
-        ASSERT_EQ(constants.Null, link.Source);
-        ASSERT_EQ(constants.Null, link.Target);
+        linkAddress = { CrudOperationsTester::TestUpdateLinkToReferenceNull(storage, linkAddress) };
         // Delete link
-        storage.Delete(linkAddress);
-        ASSERT_EQ(0, storage.Count());
-        setter = {constants.Continue, constants.Break, constants.Null};
-        storage.Each(Link{constants.Any, constants.Any, constants.Any}, setter.SetFirstFromListAndReturnTrue);
-        ASSERT_EQ(constants.Null, setter.Result());
+        CrudOperationsTester::TestDelete(storage, linkAddress);
     }
 
     template<typename TLinkAddress>
@@ -46,7 +103,7 @@ namespace Platform::Data::Doublets::Tests
         const auto constants = storage.Constants;
         Hybrid<TLinkAddress> h106E {106L, true};
         Hybrid<TLinkAddress> h107E {107L, true};
-        Hybrid<TLinkAddress> h108E {-108L, true};
+        Hybrid<TLinkAddress> h108E {108L, true};
         ASSERT_EQ(106L, h106E.AbsoluteValue());
         ASSERT_EQ(107L, h107E.AbsoluteValue());
         ASSERT_EQ(108L, h108E.AbsoluteValue());
