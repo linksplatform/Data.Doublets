4d53912d1c15ee41761e38f6321d6f02b78ae1e4
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-06 12:38:24 +0000

    Update `build_ffi.sh`

--- cpp/build_ffi.sh
+++ cpp/build_ffi.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Install rustup, cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup install nightly
 
-cd ../rust;
-cargo +nightly build --release;
-cd ffi;
+cd ../rust/ffi;
 cargo +nightly build --release;
