b615dddf2adb8315c1ff632ee619970104335d4d
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-08 15:23:14 +0600

    Use base constructors

--- cpp/Platform.Data.Doublets/Memory/United/Ffi/UnitedMemoryLinks.h
+++ cpp/Platform.Data.Doublets/Memory/United/Ffi/UnitedMemoryLinks.h
@@ -3,6 +3,8 @@ namespace Platform::Data::Doublets::Memory::United::Ffi
     template<typename TLinkAddress, typename ...TBase>
     class UnitedMemoryLinks : public UnitedMemoryLinksBase<UnitedMemoryLinks<TLinkAddress, TBase...>, TLinkAddress, TBase...>
     {
-
+        public:
+        using base = UnitedMemoryLinksBase<UnitedMemoryLinks<TLinkAddress, TBase...>, TLinkAddress, TBase...>;
+        using base::base;
     };
 }
