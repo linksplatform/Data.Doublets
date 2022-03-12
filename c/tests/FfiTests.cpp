84c1c02d911b081accf6f6803fae4229e7b0b370
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-12 20:44:57 +0600

    Use gtest

--- c/tests/FfiTests.cpp
+++ c/tests/FfiTests.cpp
@@ -1,6 +1,5 @@
+#include <gtest/gtest.h>
 #include <Platform.Data.Doublets.h>
-
-//#define DECLARE_TEST($Prefix) \
 
 UInt64Link createdLink;
 UInt64Link updatedLink;
@@ -31,7 +30,7 @@ uint64_t DeleteHandler(UInt64Link before, UInt64Link after)
     return DefaultUInt64LinksConstants.Continue;
 }
 
-int main()
+TEST(FfiTests, Test)
 {
     memset(&createdLink, 0, sizeof(createdLink));
     memset(&updatedLink, 0, sizeof(updatedLink));
@@ -40,19 +39,33 @@ int main()
     const char* tempFileName = tmpnam(NULL);
     void* uint64FfiStorage = UInt64Links_New(tempFileName);
     UInt64Links_Create(uint64FfiStorage, NULL, 0, CreateHandler);
+    ASSERT_EQ(createdLink.Index, 1);
+    ASSERT_EQ(createdLink.Source, 0);
+    ASSERT_EQ(createdLink.Target, 0);
     printf("Created link: %lu : %lu -> %lu \n", createdLink.Index, createdLink.Source, createdLink.Target);
     const uint64_t restriction[1] = {1};
     const uint64_t substitution[3] = {1, 1, 1};
     UInt64Links_Update(uint64FfiStorage, (const uint64_t*) &restriction, 1, (const uint64_t*) &substitution, 3, UpdateHandler);
     printf("Updated link: %lu : %lu -> %lu \n", updatedLink.Index, updatedLink.Source, updatedLink.Target);
+    ASSERT_EQ(updatedLink.Index, 1);
+    ASSERT_EQ(updatedLink.Source, 1);
+    ASSERT_EQ(updatedLink.Target, 1);
     UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, EachHandler);
     printf("Found link after update: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
+    ASSERT_EQ(foundLink.Index, 1);
+    ASSERT_EQ(foundLink.Source, 1);
+    ASSERT_EQ(foundLink.Target, 1);
     UInt64Links_Delete(uint64FfiStorage, (const uint64_t*) &restriction, 1, DeleteHandler);
     printf("Deleted link: %lu : %lu -> %lu \n", deletedLink.Index, deletedLink.Source, deletedLink.Target);
+    ASSERT_EQ(deletedLink.Index, 1);
+    ASSERT_EQ(deletedLink.Source, 1);
+    ASSERT_EQ(deletedLink.Target, 1);
     memset(&foundLink, 0, sizeof(foundLink));
     UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, EachHandler);
     printf("Found link after delete: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
+    ASSERT_EQ(foundLink.Index, 0);
+    ASSERT_EQ(foundLink.Source, 0);
+    ASSERT_EQ(foundLink.Target, 0);
     UInt64Links_Drop(uint64FfiStorage);
     remove(tempFileName);
-    return 0;
 }
