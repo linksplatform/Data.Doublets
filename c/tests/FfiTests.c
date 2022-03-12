ca40dd291a4321de137f4487a468302625499ab3
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-12 19:29:52 +0600

    Rename functions

--- c/tests/FfiTests.c
+++ c/tests/FfiTests.c
@@ -1,49 +1,56 @@
 #include <Platform.Data.Doublets.h>
 
+//#define DECLARE_TEST($Prefix) \
+
 UInt64Link createdLink;
 UInt64Link updatedLink;
 UInt64Link foundLink;
 UInt64Link deletedLink;
 
-uint64_t createHandler(UInt64Link before, UInt64Link after)
+uint64_t CreateHandler(UInt64Link before, UInt64Link after)
 {
     createdLink = after;
     return DefaultUInt64LinksConstants.Continue;
 }
 
-uint64_t updateHandler(UInt64Link before, UInt64Link after)
+uint64_t UpdateHandler(UInt64Link before, UInt64Link after)
 {
     updatedLink = after;
     return DefaultUInt64LinksConstants.Continue;
 }
 
-uint64_t eachHandler(UInt64Link link)
+uint64_t EachHandler(UInt64Link link)
 {
     foundLink = link;
     return DefaultUInt64LinksConstants.Continue;
 }
 
-uint64_t deleteHandler(UInt64Link before, UInt64Link after)
+uint64_t DeleteHandler(UInt64Link before, UInt64Link after)
 {
     deletedLink = before;
     return DefaultUInt64LinksConstants.Continue;
 }
 
-int main() {
+int main()
+{
+    memset(&createdLink, 0, sizeof(createdLink));
+    memset(&updatedLink, 0, sizeof(updatedLink));
+    memset(&foundLink, 0, sizeof(foundLink));
+    memset(&deletedLink, 0, sizeof(deletedLink));
     const char* tempFileName = tmpnam(NULL);
     void* uint64FfiStorage = UInt64Links_New(tempFileName);
-    UInt64Links_Create(uint64FfiStorage, NULL, 0, createHandler);
+    UInt64Links_Create(uint64FfiStorage, NULL, 0, CreateHandler);
     printf("Created link: %lu : %lu -> %lu \n", createdLink.Index, createdLink.Source, createdLink.Target);
     const uint64_t restriction[1] = {1};
     const uint64_t substitution[3] = {1, 1, 1};
-    UInt64Links_Update(uint64FfiStorage, (const uint64_t*) &restriction, 1, (const uint64_t*) &substitution, 3, updateHandler);
+    UInt64Links_Update(uint64FfiStorage, (const uint64_t*) &restriction, 1, (const uint64_t*) &substitution, 3, UpdateHandler);
     printf("Updated link: %lu : %lu -> %lu \n", updatedLink.Index, updatedLink.Source, updatedLink.Target);
-    UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, eachHandler);
+    UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, EachHandler);
     printf("Found link after update: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
-    UInt64Links_Delete(uint64FfiStorage, (const uint64_t*) &restriction, 1, deleteHandler);
+    UInt64Links_Delete(uint64FfiStorage, (const uint64_t*) &restriction, 1, DeleteHandler);
     printf("Deleted link: %lu : %lu -> %lu \n", deletedLink.Index, deletedLink.Source, deletedLink.Target);
     memset(&foundLink, 0, sizeof(foundLink));
-    UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, eachHandler);
+    UInt64Links_Each(uint64FfiStorage, (const uint64_t*) &restriction, 1, EachHandler);
     printf("Found link after delete: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
     UInt64Links_Drop(uint64FfiStorage);
     remove(tempFileName);
