1afbd370bce07795117e21853bf1b150106be642
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-12 11:25:00 +0600

    Update `FfiTest`

--- c/Platform.Data.Doublets.Tests/FfiTest.c
+++ c/Platform.Data.Doublets.Tests/FfiTest.c
@@ -1,8 +1,51 @@
 #include <Platform.Data.Doublets.h>
+#include <stdio.h>
+
+UInt64Link createdLink;
+UInt64Link updatedLink;
+UInt64Link foundLink;
+UInt64Link deletedLink;
+
+uint64_t createHandler(UInt64Link before, UInt64Link after)
+{
+    createdLink = after;
+    return DefaultUInt64LinksConstants.$continue;
+}
+
+uint64_t updateHandler(UInt64Link before, UInt64Link after)
+{
+    updatedLink = after;
+    return DefaultUInt64LinksConstants.$continue;
+}
+
+uint64_t eachHandler(UInt64Link link)
+{
+    foundLink = link;
+    return DefaultUInt64LinksConstants.$continue;
+}
+
+uint64_t deleteHandler(UInt64Link before, UInt64Link after)
+{
+    deletedLink = before;
+    return DefaultUInt64LinksConstants.$continue;
+}
 
 int main() {
-    void* ffiStoragePtr = UInt64Links_New("db.links");
-    UInt64Links_Drop(ffiStoragePtr);
-    free(ffiStoragePtr);
+    char* tempFileName = tmpnam(NULL);
+    void* uint64FfiStorage = UInt64Links_New(tempFileName);
+    UInt64Links_Create(uint64FfiStorage, NULL, 0, createHandler);
+    printf("Created link: %lu : %lu -> %lu \n", createdLink.Index, createdLink.Source, createdLink.Target);
+    const uint64_t restriction = {1};
+    const uint64_t substitution = {1, 1, 1};
+    UInt64Links_Update(uint64FfiStorage, &restriction, 1, &substitution, 3, updateHandler);
+    printf("Updated link: %lu : %lu -> %lu \n", updatedLink.Index, updatedLink.Source, updatedLink.Target);
+    UInt64Links_Each(uint64FfiStorage, &restriction, 1, eachHandler);
+    printf("Found link after update: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
+    UInt64Links_Delete(uint64FfiStorage, &restriction, 1, deleteHandler);
+    printf("Deleted link: %lu : %lu -> %lu \n", deletedLink.Index, deletedLink.Source, deletedLink.Target);
+    UInt64Links_Each(uint64FfiStorage, &restriction, 1, eachHandler);
+    printf("Found link after delete: %lu : %lu -> %lu \n", foundLink.Index, foundLink.Source, foundLink.Target);
+    UInt64Links_Drop(uint64FfiStorage);
+    free(uint64FfiStorage);
     return 0;
 }
