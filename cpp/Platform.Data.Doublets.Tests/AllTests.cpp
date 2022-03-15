3d61ee61b56198d7cb63823336a8f38bd50b3b26
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-15 12:59:10 +0600

    Define constants and TLinkAddress in AllTests

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -2,5 +2,7 @@
 #include <Platform.Data.Doublets.h>
 #include "ILinksTestExtensions.h"
 
+using TLinkAddress = std::uint64_t;
+constexpr Platform::Data::LinksConstants<TLinkAddress> constants {true};
 #include "GenericLinksTests.cpp"
 //#include "UnitedMemoryLinksFfiTests.cpp"
