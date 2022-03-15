f4a8d0656c3646e21d5db146d9f4b51149787281
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-15 11:59:48 +0600

    Remove unused code

--- cpp/Platform.Data.Doublets/ILinksExtensions.h
+++ cpp/Platform.Data.Doublets/ILinksExtensions.h
@@ -642,12 +642,6 @@ namespace Platform::Data::Doublets
     }
 
     template<typename TLinkAddress>
-    bool Exists(auto&& storage, TLinkAddress source, TLinkAddress target)
-    {
-        return storage.Count(storage.Constants.Any, source, target) != 0;
-    }
-
-    template<typename TLinkAddress>
     static bool Exists(auto&& storage, TLinkAddress source, TLinkAddress target)
     {
         return storage.Count(Link{storage.Constants.Any, source, target}) > 0;
