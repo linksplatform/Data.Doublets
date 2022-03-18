cc4fb3753652234f6fb8fb7ee334b101de98448d
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-18 19:09:15 +0600

    Do not use declare constants and TLinkAddress in AllTests

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -2,7 +2,5 @@
 #include <Platform.Data.Doublets.h>
 #include "ILinksTestExtensions.h"
 
-using TLinkAddress = std::uint64_t;
-constexpr Platform::Data::LinksConstants<TLinkAddress> constants {true};
 #include "GenericLinksTests.cpp"
 //#include "UnitedMemoryLinksFfiTests.cpp"
