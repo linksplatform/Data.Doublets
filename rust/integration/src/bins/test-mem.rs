98ce0f8ee9ebb07204291766b27c17ce5f347435
uselessgoddess <dodickgod@gmail.com>
2022-03-20 14:32:54 +0300

    Add valgrind test

--- rust/integration/src/bins/test-mem.rs
+++ rust/integration/src/bins/test-mem.rs
@@ -6,6 +6,6 @@ use doublets::test_extensions::ILinksTestExtensions;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut links = Store::<usize, _>::new(GlobalMem::new()?)?;
-    links.test_random_creations_and_deletions()?;
+    links.test_random_creations_and_deletions(1000)?;
     Ok(())
 }
