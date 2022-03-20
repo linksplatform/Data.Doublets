4130bb7d1f4385950d2daa3532a0b26d6d48feed
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-20 13:52:38 +0600

    Add `UnitedMemoryLinks` benchmark

--- cpp/Platform.Data.Doublets.Benchmarks/CreateMillionPointsBenchmarks.cpp
+++ cpp/Platform.Data.Doublets.Benchmarks/CreateMillionPointsBenchmarks.cpp
@@ -1,12 +1,10 @@
 namespace Platform::Data::Doublets::Benchmarks
 {
     using TLinkAddress = std::uint64_t;
-    static void BM_CreateMillionPoints(benchmark::State& state)
+    static void BM_CreateMillionPointsFfi(benchmark::State& state)
     {
         using namespace Platform::Memory;
-        using namespace Platform::Data::Doublets::Memory::United::Generic;
         using namespace Platform::Data::Doublets::Memory::United;
-        using namespace Platform::Collections;
         std::string tempFilePath { std::tmpnam(nullptr) };
         Expects(!Collections::IsWhiteSpace(tempFilePath));
         try
@@ -17,7 +15,7 @@ namespace Platform::Data::Doublets::Benchmarks
             {
                 for (std::size_t i = 0; i < state.range(0); ++i)
                 {
-                    CreatePoint<TLinkAddress>(ffiStorage);
+                    CreatePoint(ffiStorage);
                 }
             }
         }
@@ -29,5 +27,34 @@ namespace Platform::Data::Doublets::Benchmarks
         std::remove(tempFilePath.c_str());
     }
 
+    using TLinkAddress = std::uint64_t;
+    static void BM_CreateMillionPoints(benchmark::State& state)
+    {
+        using namespace Platform::Memory;
+        using namespace Platform::Data::Doublets::Memory::United::Generic;
+        std::string tempFilePath { std::tmpnam(nullptr) };
+        Expects(!Collections::IsWhiteSpace(tempFilePath));
+        try
+        {
+            constexpr LinksConstants<TLinkAddress> constants {true};
+            FileMappedResizableDirectMemory memory {tempFilePath};
+            UnitedMemoryLinks<TLinkAddress, FileMappedResizableDirectMemory, constants> storage {std::move(memory)};
+            for (auto _ : state)
+            {
+                for (std::size_t i = 0; i < state.range(0); ++i)
+                {
+                    CreatePoint(storage);
+                }
+            }
+        }
+        catch (...)
+        {
+            std::remove(tempFilePath.c_str());
+            throw;
+        }
+        std::remove(tempFilePath.c_str());
+    }
+
+    BENCHMARK(BM_CreateMillionPointsFfi)->Arg(1000000);
     BENCHMARK(BM_CreateMillionPoints)->Arg(1000000);
 }
