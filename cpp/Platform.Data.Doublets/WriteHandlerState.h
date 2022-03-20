9e6defc37a8019ced1c35f5f3e66e51267a09154
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-20 13:50:30 +0600

    Use `TStorage` template parameter in `WriteHandlerState`. Add `Handle()`

--- cpp/Platform.Data.Doublets/WriteHandlerState.h
+++ cpp/Platform.Data.Doublets/WriteHandlerState.h
@@ -1,33 +1,34 @@
 namespace Platform::Data::Doublets
 {
     using namespace Platform::Interfaces;
-    template<typename TLinkAddress, typename THandlerParameter>
+    template<typename TStorage>
     class WriteHandlerState
     {
     private:
-        TLinkAddress _result;
-        TLinkAddress _break;
+        typename TStorage::LinkAddressType _break;
     public:
-        std::function<TLinkAddress(THandlerParameter, THandlerParameter)> Handler;
+        typename TStorage::LinkAddressType Result;
+        std::function<typename TStorage::LinkAddressType(typename TStorage::HandlerParameterType, typename TStorage::HandlerParameterType)> Handler;
 
-        WriteHandlerState(TLinkAddress $continue, TLinkAddress $break, auto&& handler) : _result{$continue}, _break{$break}, Handler{handler} {}
+        WriteHandlerState(typename TStorage::LinkAddressType $continue, typename TStorage::LinkAddressType $break, auto&& handler) :
+            Result{$continue}, _break{$break}, Handler{handler} {}
 
-        TLinkAddress Apply(TLinkAddress result)
+        typename TStorage::LinkAddressType Apply(typename TStorage::LinkAddressType result)
         {
-            _result = result;
+            Result = result;
             return result;
         }
 
-//        TLinkAddress Handle () (auto&& ...args)
-//        {
-//            if(nullptr == _handler)
-//            {
-//                return _result;
-//            }
-//            else
-//            {
-//                return _handler(std::forward<decltype(args)>(args)...);
-//            }
-//        }
+        typename TStorage::LinkAddressType Handle(auto ...args)
+        {
+            if(nullptr == Handler)
+            {
+                return Result;
+            }
+            else
+            {
+                return Handler(std::forward<decltype(args)>(args)...);
+            }
+        }
     };
 }
