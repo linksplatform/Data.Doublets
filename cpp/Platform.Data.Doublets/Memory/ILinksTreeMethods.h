09829d6f16e06fc6a5add7cb476f727a1c02ff46
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-23 16:30:10 +0600

    Add `using ReadHandlerType` and use it in EachUsage for handler type

--- cpp/Platform.Data.Doublets/Memory/ILinksTreeMethods.h
+++ cpp/Platform.Data.Doublets/Memory/ILinksTreeMethods.h
@@ -6,11 +6,12 @@
         using OptionsType = TLinksOptions;
         using LinkAddressType = OptionsType::LinkAddressType;
         using LinkType = OptionsType::LinkAddressType;
+        using ReadHandlerType = OptionsType::ReadHandlerType;
         virtual LinkAddressType CountUsages(LinkAddressType root) = 0;
 
         virtual LinkAddressType Search(LinkAddressType source, LinkAddressType target) = 0;
 
-        virtual LinkAddressType EachUsage(LinkAddressType root, const std::function<LinkAddressType(const LinkType&)>& handler) = 0;
+        virtual LinkAddressType EachUsage(LinkAddressType root, const ReadHandlerType& handler) = 0;
 
         virtual void Detach(LinkAddressType& root, LinkAddressType linkIndex) = 0;
 
