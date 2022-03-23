b7e6557fdc4d04284da63f9daeff9859464b80a0
FreePhoenix888 <freephoenix888@gmail.com>
2022-03-23 16:18:31 +0600

    Pass TLinksOptions to ILinksTreeMethods

--- cpp/Platform.Data.Doublets/Memory/ILinksTreeMethods.h
+++ cpp/Platform.Data.Doublets/Memory/ILinksTreeMethods.h
@@ -1,16 +1,19 @@
 ﻿namespace Platform::Data::Doublets::Memory
 {
-    template<typename TLink>
+    template<typename TLinksOptions>
     struct ILinksTreeMethods
     {
-        virtual TLink CountUsages(TLink root) = 0;
+        using OptionsType = TLinksOptions;
+        using LinkAddressType = OptionsType::LinkAddressType;
+        using LinkType = OptionsType::LinkAddressType;
+        virtual LinkAddressType CountUsages(LinkAddressType root) = 0;
 
-        virtual TLink Search(TLink source, TLink target) = 0;
+        virtual LinkAddressType Search(LinkAddressType source, LinkAddressType target) = 0;
 
-        virtual TLink EachUsage(TLink root, const std::function<TLink(const std::vector<TLink>&)>& handler) = 0;
+        virtual LinkAddressType EachUsage(LinkAddressType root, const std::function<LinkAddressType(const LinkType&)>& handler) = 0;
 
-        virtual void Detach(TLink& root, TLink linkIndex) = 0;
+        virtual void Detach(LinkAddressType& root, LinkAddressType linkIndex) = 0;
 
-        virtual void Attach(TLink& root, TLink linkIndex) = 0;
+        virtual void Attach(LinkAddressType& root, LinkAddressType linkIndex) = 0;
     };
 }
