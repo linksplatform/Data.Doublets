be6e41a9a3e655388ce00ffb3bef7e1d21430036
Konstantin Dyachenko <drakonard@gmail.com>
2022-04-03 17:35:05 +0000

    Updated to be compatible with GitPod.

--- csharp/install-dotnet.sh
+++ csharp/install-dotnet.sh
@@ -1,4 +1,4 @@
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
