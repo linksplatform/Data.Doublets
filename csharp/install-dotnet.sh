c576fb56f33bf33d4b5633592b63ddf823ec8710
Konstantin Dyachenko <drakonard@gmail.com>
2022-04-03 17:32:55 +0000

    Script for installing dotnet.

---
+++ csharp/install-dotnet.sh
@@ -0,0 +1,15 @@
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-6.0
+
+sudo apt-get install -y dotnet-runtime-6.0
