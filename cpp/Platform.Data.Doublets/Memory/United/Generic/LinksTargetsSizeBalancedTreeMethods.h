e4f5ebe4a48f9435a3d6534578f0c17233d8f36f
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-09 11:47:00 +0600

    Add using LinkType

--- cpp/Platform.Data.Doublets/Memory/United/Generic/LinksTargetsSizeBalancedTreeMethods.h
+++ cpp/Platform.Data.Doublets/Memory/United/Generic/LinksTargetsSizeBalancedTreeMethods.h
@@ -6,6 +6,7 @@
     {
         using base = LinksSizeBalancedTreeMethodsBase<LinksTargetsSizeBalancedTreeMethods<TLinksOptions>, TLinksOptions>;
         using typename base::LinkAddressType;
+        using typename base::LinkType;
         using typename base::ReadHandlerType;
 
         public: LinksTargetsSizeBalancedTreeMethods(std::byte* storage, std::byte* header) : base(storage, header) { }
