d647a0036ce07de9cd57050b68910618266f06ae
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-14 11:42:11 +0600

    Translate LinksConstantsTests

--- cpp/Platform.Data.Doublets.Tests/LinksConstantsTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/LinksConstantsTests.cpp
@@ -3,12 +3,16 @@
     using TLinkAddress = std::uint64_t;
     TEST(LinksConstantsTests, ExternalReferencesTest)
     {
-        Ranges::Range possibleInternalReferencesRange {1, std::numeric_limits<std::int64_t>::max()};
-        Ranges::Range possibleExternalReferencesRange {std::numeric_limits<std::int64_t>::max() + 1UL, std::numeric_limits<TLinkAddress>::max()};
-        LinksConstants<TLinkAddress> constants {possibleInternalReferencesRange, possibleExternalReferencesRange};
-        Hybrid<TLinkAddress> minimum {1, isExternal: true};
-        Hybrid<TLinkAddress> maximum {std::numeric_limits<TLinkAddress>::max(), isExternal: true};
-        Expects(constants.IsExternalReference(minimum))
-        Expects(constants.IsExternalReference(maximum));
+        using namespace Platform::Ranges;
+        constexpr Ranges::Range possibleInternalReferencesRange {1, std::numeric_limits<TLinkAddress>::max()};
+        constexpr Ranges::Range possibleExternalReferencesRange {std::numeric_limits<TLinkAddress>::max() + 1UL, std::numeric_limits<TLinkAddress>::max()};
+        constexpr LinksConstants<TLinkAddress> constants {possibleInternalReferencesRange, possibleExternalReferencesRange};
+        auto isExternal {true};
+        Hybrid<TLinkAddress> minimum {1, isExternal};
+        Hybrid<TLinkAddress> maximum {std::numeric_limits<TLinkAddress>::max(), true};
+        bool isMinimumExternalReference = IsExternalReference<TLinkAddress, constants>(minimum.Value);
+        bool isMaximumExternalReference = IsExternalReference<TLinkAddress, constants>(minimum.Value);
+        ASSERT_TRUE(isMinimumExternalReference);
+        ASSERT_TRUE(isMaximumExternalReference);
     };
 }
