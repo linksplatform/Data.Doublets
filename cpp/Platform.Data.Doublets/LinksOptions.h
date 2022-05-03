85e07432aef76ad635de18f66a1f657033d47d3b
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-03 17:04:48 +0600

    Move Constants and add TLink template parameters

--- cpp/Platform.Data.Doublets/LinksOptions.h
+++ cpp/Platform.Data.Doublets/LinksOptions.h
@@ -1,14 +1,14 @@
 namespace Platform::Data::Doublets
 {
 
-    template<typename TLinkAddress = std::uint64_t, typename TWriteHandler = std::function<TLinkAddress(std::vector<TLinkAddress>, std::vector<TLinkAddress>)>, typename TReadHandler = std::function<TLinkAddress(std::vector<TLinkAddress>)>, LinksConstants<TLinkAddress> VConstants = LinksConstants<TLinkAddress>{true}>
-    struct LinksOptions : Platform::Data::LinksOptions<TLinkAddress, TWriteHandler, TReadHandler, VConstants>
+    template<typename TLinkAddress = std::uint64_t, LinksConstants<TLinkAddress> VConstants = LinksConstants<TLinkAddress>{true}, typename TLink = std::vector<TLinkAddress>, typename TReadHandler = std::function<TLinkAddress(std::vector<TLinkAddress>)>, typename TWriteHandler = std::function<TLinkAddress(std::vector<TLinkAddress>, std::vector<TLinkAddress>)>>
+    struct LinksOptions : Platform::Data::LinksOptions<TLinkAddress, VConstants, TLink, TReadHandler, TWriteHandler>
     {
-        using base = Platform::Data::LinksOptions<TLinkAddress, TWriteHandler, TReadHandler, VConstants>;
+        using base = Platform::Data::LinksOptions<TLinkAddress, VConstants, TLink, TReadHandler, TWriteHandler>;
         using base::LinkAddressType;
+        using base::Constants;
         using base::LinkType;
         using base::WriteHandlerType;
         using base::ReadHandlerType;
-        using base::Constants;
     };
 }
