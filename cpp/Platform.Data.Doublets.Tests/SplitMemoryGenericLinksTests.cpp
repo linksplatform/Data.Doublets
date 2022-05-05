3fe01cf8ca53c846ff6d19cbf226ed9f0783322b
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-05 20:15:45 +0600

    Update tests

--- cpp/Platform.Data.Doublets.Tests/SplitMemoryGenericLinksTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/SplitMemoryGenericLinksTests.cpp
@@ -1,45 +1,47 @@
 ﻿namespace Platform::Data::Doublets::Tests
 {
-    TEST(SplitMemoryGenericLinksTests, CrudTest)
+    template <typename TLinkAddress>
+    static void Using(auto&& action)
     {
-        Using<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations() });
-        Using<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations() });
-        Using<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations() });
-        Using<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations() });
-    }
-
-    TEST(SplitMemoryGenericLinksTests, RawNumbersCrudTest)
-    {
-        UsingWithExternalReferences<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations() });
-        UsingWithExternalReferences<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations() });
-        UsingWithExternalReferences<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations() });
-        UsingWithExternalReferences<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations() });
-    }
-
-    TEST(SplitMemoryGenericLinksTests, MultipleRandomCreationsAndDeletionsTest)
-    {
-        Using<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution() }.TestMultipleRandomCreationsAndDeletions(16));
-        Using<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution() }.TestMultipleRandomCreationsAndDeletions(100));
-        Using<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution() }.TestMultipleRandomCreationsAndDeletions(100));
-        Using<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution() }.TestMultipleRandomCreationsAndDeletions(100));
-    }
-
-    private: template <typename TLinkAddress>
-    static void Using(std::invocable auto&& action)
-    {
+        using namespace Platform::Memory;
+        using namespace Platform::Data::Doublets::Memory::Split::Generic;
         HeapResizableDirectMemory dataMemory { };
         HeapResizableDirectMemory indexMemory { };
         SplitMemoryLinks<TLinkAddress> memory { dataMemory, indexMemory };
         action(memory);
     }
 
-    private: template <typename TLinkAddress>
-    static void Using(std::invocable auto&& action)
+    template <typename TLinkAddress>
+    static void Using(auto&& action)
     {
         HeapResizableDirectMemory dataMemory { };
         HeapResizableDirectMemory indexMemory { };
         LinksConstants<TLinkAddress> constants { true };
         SplitMemoryLinks<TLinkAddress> memory { dataMemory, indexMemory, SplitMemoryLinks<TLinkAddress>::DefaultIndexSize, constants };
         action(memory);
+    }
+
+    TEST(SplitMemoryGenericLinksTests, CrudTest)
+    {
+        Using<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations(); });
+        Using<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations(); });
+        Using<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations(); });
+        Using<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.TestCrudOperations(); });
+    }
+
+    TEST(SplitMemoryGenericLinksTests, RawNumbersCrudTest)
+    {
+        UsingWithExternalReferences<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations(); });
+        UsingWithExternalReferences<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations(); });
+        UsingWithExternalReferences<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations(); });
+        UsingWithExternalReferences<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.TestRawNumbersCrudOperations(); });
+    }
+
+    TEST(SplitMemoryGenericLinksTests, MultipleRandomCreationsAndDeletionsTest)
+    {
+        Using<std::uint8_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution(); }.TestMultipleRandomCreationsAndDeletions(16));
+        Using<std::uint16_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution(); }.TestMultipleRandomCreationsAndDeletions(100));
+        Using<std::uint32_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution(); }.TestMultipleRandomCreationsAndDeletions(100));
+        Using<std::uint64_t>([] (Interfaces::CArray auto storage) { return storage.DecorateWithAutomaticUniquenessAndUsagesResolution(); }.TestMultipleRandomCreationsAndDeletions(100));
     }
 }
