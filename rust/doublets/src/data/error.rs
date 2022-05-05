688ef5eb00df91467ca5d46b545d8b6dba16e0fe
uselessgoddess <dodickgod@gmail.com>
2022-05-05 16:19:02 +0300

    Alpha.13 - use platform-mem.5

--- rust/doublets/src/data/error.rs
+++ rust/doublets/src/data/error.rs
@@ -6,16 +6,16 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum LinksError<T: LinkType> {
-    #[error("link [{0}] does not exist.")]
+    #[error("link {0} does not exist.")]
     NotExists(T),
 
-    #[error("link [{0}] has dependencies")]
-    HasDeps(Link<T>),
+    #[error("link {0:?} has dependencies")]
+    HasUsages(Vec<Link<T>>),
 
-    #[error("link [{0}] already exists")]
+    #[error("link {0} already exists")]
     AlreadyExists(Doublet<T>),
 
-    #[error("limit for the number of links in the storage has been reached ({0})")]
+    #[error("limit for the number of links in the storage has been reached: {0}")]
     LimitReached(T),
 
     #[error("unable to allocate memory for links storage: `{0}`")]
