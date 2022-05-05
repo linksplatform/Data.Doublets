38fc2a51405dcda91ab93269c8ef847d63511925
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-05 18:31:56 +0600

    Use only split tests

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -2,7 +2,7 @@
 #include <Platform.Data.Doublets.h>
 #include "ILinksTestExtensions.h"
 
-#include "GenericLinksTests.cpp"
+//#include "GenericLinksTests.cpp"
 //#include "UnitedMemoryLinksFfiTests.cpp"
 
 #include "SplitMemoryGenericLinksTests.cpp"
