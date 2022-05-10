bc0564f3498961dc4c8abae7e6895966cefca36c
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-10 12:54:45 +0600

    Add default template args

--- cpp/Platform.Data.Doublets/Memory/Split/Generic/SplitMemoryLinks.h
+++ cpp/Platform.Data.Doublets/Memory/Split/Generic/SplitMemoryLinks.h
@@ -1,15 +1,16 @@
 namespace Platform::Data::Doublets::Memory::Split::Generic
 {
     using namespace Platform::Interfaces;
+    using namespace Platform::Memory;
     template<
         typename TLinksOptions,
-        typename TMemory,
-        typename TInternalSourcesTreeMethods,
-        typename TInternalTargetsTreeMethods,
-        typename TExternalSourcesTreeMethods,
-        typename TExternalTargetsTreeMethods,
-        typename TInternalLinksSourcesLinkedTreeMethods,
-        typename TUnusedLinksTreeMethods,
+        typename TMemory = FileMappedResizableDirectMemory,
+        typename TInternalSourcesTreeMethods = InternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>,
+        typename TInternalTargetsTreeMethods = InternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>,
+        typename TExternalSourcesTreeMethods = ExternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>,
+        typename TExternalTargetsTreeMethods = ExternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>,
+        typename TInternalLinksSourcesLinkedTreeMethods = InternalLinksSourcesLinkedListMethods<TLinksOptions>,
+        typename TUnusedLinksTreeMethods = UnusedLinksListMethods<TLinksOptions>,
         typename... TBase>
     class SplitMemoryLinks : SplitMemoryLinksBase<SplitMemoryLinks<TLinksOptions, TMemory, TInternalSourcesTreeMethods, TInternalTargetsTreeMethods, TExternalSourcesTreeMethods, TExternalTargetsTreeMethods, TInternalLinksSourcesLinkedTreeMethods, TUnusedLinksTreeMethods, TBase...>, TLinksOptions, TMemory, TInternalSourcesTreeMethods, TInternalTargetsTreeMethods, TExternalSourcesTreeMethods, TExternalTargetsTreeMethods, TInternalLinksSourcesLinkedTreeMethods, TUnusedLinksTreeMethods, TBase...>
     {
