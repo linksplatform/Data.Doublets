a7ade61fa74af3708fa8220b159c39796e83582e
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-11 19:54:31 +0600

    Revert "Use this"
    This reverts commit e79bfe7d736ad4b84ad042df0576e5e9797ca027.

--- cpp/Platform.Data.Doublets/Memory/Split/Generic/SplitMemoryLinksBase.h
+++ cpp/Platform.Data.Doublets/Memory/Split/Generic/SplitMemoryLinksBase.h
@@ -826,7 +826,7 @@ namespace Platform::Data::Doublets::Memory::Split::Generic
             auto freeLink = header.FirstFreeLink;
             if (freeLink != Constants.Null)
             {
-                this->Detach(freeLink);
+                UnusedLinksListMethods->Detach(freeLink);
             }
             else
             {
