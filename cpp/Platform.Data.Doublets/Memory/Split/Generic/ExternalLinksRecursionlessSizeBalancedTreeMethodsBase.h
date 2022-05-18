a9bf70a06ad8b6bba6b3f60363f83cf7826ae8a3
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-18 12:12:06 +0600

    Do not inherig ILinksTreeMethods

--- cpp/Platform.Data.Doublets/Memory/Split/Generic/ExternalLinksRecursionlessSizeBalancedTreeMethodsBase.h
+++ cpp/Platform.Data.Doublets/Memory/Split/Generic/ExternalLinksRecursionlessSizeBalancedTreeMethodsBase.h
@@ -2,7 +2,7 @@
 {
     using namespace Platform::Collections::Methods::Trees;
     template<typename TSelf, typename TLinksOptions>
-    class ExternalLinksRecursionlessSizeBalancedTreeMethodsBase : public RecursionlessSizeBalancedTreeMethods<TSelf, typename TLinksOptions::LinkAddressType>, ILinksTreeMethods<typename TLinksOptions::LinkAddressType>
+    class ExternalLinksRecursionlessSizeBalancedTreeMethodsBase : public RecursionlessSizeBalancedTreeMethods<TSelf, typename TLinksOptions::LinkAddressType>/*, ILinksTreeMethods<typename TLinksOptions::LinkAddressType>*/
     {
     public: using LinksOptionsType = TLinksOptions;
     public: static constexpr auto Constants = LinksOptionsType::Constants;
