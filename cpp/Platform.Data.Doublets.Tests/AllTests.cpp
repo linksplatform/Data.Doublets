d64886eff07483a02dfc4cd444d89521f3fdbe09
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-25 18:43:49 +0600

    Add dynamic polymorphism tests

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -3,7 +3,9 @@
 #include "ILinksTestExtensions.h"
 
 #include "GenericLinksTests.cpp"
+#include "DynamicPolymorphismGenericLinksTests.cpp"
 #include "LinksConstantsTests.cpp"
 #include "ResizableDirectMemoryLinksTests.cpp"
 #include "SplitMemoryGenericLinksTests.cpp"
+#include "DynamicPolymorphismSplitMemoryGenericLinksTests.cpp"
 
