5ea19140343c351e1b20d543459af07e4a8cdb81
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-25 18:35:08 +0600

    Update tests

---
+++ cpp/Platform.Data.Doublets.Tests/DynamicPolymorphismSplitMemoryGenericLinksTests.cpp
@@ -0,0 +1,71 @@
+﻿namespace Platform::Data::Doublets::Tests
+{
+    template<typename TStorage>
+    static void UsingStorage(auto&& action)
+    {
+      using namespace Platform::Memory;
+      using namespace Platform::Data::Doublets::Memory::Split::Generic;
+      TStorage storage{ HeapResizableDirectMemory{ }, HeapResizableDirectMemory{ } };
+      action(storage);
+    }
+
+    template <typename TLinkAddress>
+    static void UsingStorageWithExternalReference(auto&& action)
+    {
+      using LinksOptionsType = LinksOptions<TLinkAddress>;
+      using StorageType = SplitMemoryLinks<LinksOptionsType, HeapResizableDirectMemory, InternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, InternalLinksSourcesLinkedListMethods<TLinksOptions>, InternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, UnusedLinksListMethods<TLinksOptions>, Doublets::ILinks<LinksOptionsType>>;
+      UsingStorage<StorageType>(action)
+    }
+
+    template <typename TLinkAddress>
+    static void UsingStorageWithoutExternalReferences(auto&& action)
+    {
+      using LinksOptionsType = LinksOptions<TLinkAddress, LinksConstants{false}>;
+      using StorageType = SplitMemoryLinks<LinksOptionsType, HeapResizableDirectMemory, InternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, InternalLinksSourcesLinkedListMethods<TLinksOptions>, InternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, UnusedLinksListMethods<TLinksOptions>, Doublets::ILinks<LinksOptionsType>>;
+      UsingStorage<StorageType>(action);
+    }
+
+    template <typename TLinkAddress>
+    static void UsingDecoratedWithAutomaticUniquenessAndUsagesResolution(auto&& action)
+    {
+        using namespace Platform::Memory;
+        using namespace Platform::Data::Doublets::Memory::Split::Generic;
+        using namespace Platform::Data::Doublets::Decorators;
+        using LinksOptionsType = LinksOptions<TLinkAddress>;
+        using StorageType = SplitMemoryLinks<LinksOptionsType, HeapResizableDirectMemory, InternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, InternalLinksSourcesLinkedListMethods<TLinksOptions>, InternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksSourcesSizeBalancedTreeMethods<TLinksOptions>, ExternalLinksTargetsSizeBalancedTreeMethods<TLinksOptions>, UnusedLinksListMethods<TLinksOptions>, Doublets::ILinks<LinksOptionsType>>;
+        using DecoratedStorageType = LinksDecoratedWithAutomaticUniquenessAndUsagesResolution<StorageType>;
+        UsingStorage<DecoratedStorageType>(action)
+    }
+
+    TEST(SplitMemoryGenericLinksTests, CrudTest)
+    {
+      UsingStorageWithExternalReference<std::uint8_t>(
+          [](auto &&storage) { return TestCrudOperations(storage); });
+        UsingStorageWithExternalReference<std::uint16_t>(
+            [](auto &&storage) { return TestCrudOperations(storage); });
+        UsingStorageWithExternalReference<std::uint32_t>(
+            [](auto &&storage) { return TestCrudOperations(storage); });
+        UsingStorageWithExternalReference<std::uint64_t>(
+            [](auto &&storage) { return TestCrudOperations(storage); });
+    }
+
+    TEST(SplitMemoryGenericLinksTests, RawNumbersCrudTest)
+    {
+      UsingStorageWithoutExternalReferences<std::uint8_t>(
+          [](auto &&storage) { return TestRawNumbersCrudOperations(storage); });
+      UsingStorageWithoutExternalReferences<std::uint16_t>(
+          [](auto &&storage) { return TestRawNumbersCrudOperations(storage); });
+      UsingStorageWithoutExternalReferences<std::uint32_t>(
+          [](auto &&storage) { return TestRawNumbersCrudOperations(storage); });
+      UsingStorageWithoutExternalReferences<std::uint64_t>(
+          [](auto &&storage) { return TestRawNumbersCrudOperations(storage); });
+    }
+
+    TEST(SplitMemoryGenericLinksTests, MultipleRandomCreationsAndDeletionsTest)
+    {
+        UsingDecoratedWithAutomaticUniquenessAndUsagesResolution<std::uint8_t>([] (auto&& storage) { return  TestMultipleRandomCreationsAndDeletions(storage,16); });
+        UsingDecoratedWithAutomaticUniquenessAndUsagesResolution<std::uint16_t>([] (auto&& storage) { return  TestMultipleRandomCreationsAndDeletions(storage,100); });
+        UsingDecoratedWithAutomaticUniquenessAndUsagesResolution<std::uint32_t>([] (auto&& storage) { return  TestMultipleRandomCreationsAndDeletions(storage,100); });
+        UsingDecoratedWithAutomaticUniquenessAndUsagesResolution<std::uint64_t>([] (auto&& storage) { return  TestMultipleRandomCreationsAndDeletions(storage,100); });
+    }
+}
