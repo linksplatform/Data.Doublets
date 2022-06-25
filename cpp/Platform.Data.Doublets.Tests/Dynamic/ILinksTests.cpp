0953a54bde9304e0620b9e72fa5533b6306817b2
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-25 19:18:52 +0600

    Add ILinksTests

---
+++ cpp/Platform.Data.Doublets.Tests/Dynamic/ILinksTests.cpp
@@ -0,0 +1,20 @@
+namespace Platform::Data::Doublets::Tests
+{
+  template<typaneme TLinkAddress>
+  static void FunctionWithILinksParameter(Doublets::ILinks<TLinkAddress>)
+  {
+
+  }
+
+  TEST(ILinksTests, CastToILinksTest)
+  {
+    using namespace Platform::Memory;
+    using namespace Platform::Data::Doublets::Memory::United::Generic;
+    using namespace Platform::Data::Doublets::Memory::United;
+    using LinksOptionsType = LinksOptions<TLinkAddress>;
+    using StorageType = UnitedMemoryLinks<LinksOptionsType, HeapResizableDirectMemory, LinksSourcesSizeBalancedTreeMethods<LinksOptionsType>, LinksTargetsSizeBalancedTreeMethods<LinksOptionsType>, UnusedLinksListMethods<TLinkAddress>, Doublets::ILinks<LinksOptionsType>>;
+    UsingStorage<StorageType>(action);
+    StorageType storage { HeapResizableDirectMemory{}, HeapResizableDirectMemory {}};
+    FunctionWithILinksParameter(storage);
+  }
+}
