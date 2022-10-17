92f29f36066e833e757f3759aed7cf44a74e295e
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-10-17 22:42:30 +0500

    Update AllTests.cpp

--- cpp/Platform.Data.Doublets.Tests/AllTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/AllTests.cpp
@@ -2,9 +2,9 @@
 #include <Platform.Data.Doublets.h>
 #include "ILinksTestExtensions.h"
 
+#include "LinksConstantsTests.cpp"
+#include "ResizableDirectMemoryLinksTests.cpp"
 #include "Static/GenericLinksTests.cpp"
-#include "Static/LinksConstantsTests.cpp"
-#include "Static/ResizableDirectMemoryLinksTests.cpp"
 #include "Static/SplitMemoryGenericLinksTests.cpp"
 #include "Dynamic/GenericLinksTests.cpp"
 #include "Dynamic/SplitMemoryGenericLinksTests.cpp"
