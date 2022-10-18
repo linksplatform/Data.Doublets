e4d91a1ea23c2d8b92f533867958c511b40fe910
FreePhoenix888 <freephoenix888@gmail.com>
2022-10-18 18:42:59 +0500

    Fix typo

--- cpp/Platform.Data.Doublets.Tests/Dynamic/ILinksTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/Dynamic/ILinksTests.cpp
@@ -1,7 +1,7 @@
 namespace Platform::Data::Doublets::Tests::Dynamic::ILinksTests
 {
   template<typename TLinksOptions>
-  static void FunctionWithILinksParameter(Doublets::ILinks<TLinksOptions>)
+  static void FunctionWithILinksParameter(Doublets::ILinks<TLinksOptions>& storage)
   {
 
   }
@@ -11,6 +11,7 @@ namespace Platform::Data::Doublets::Tests::Dynamic::ILinksTests
     using namespace Platform::Memory;
     using namespace Platform::Data::Doublets::Memory::United::Generic;
     using namespace Platform::Data::Doublets::Memory::United;
+    using TLinkAddress = std::uint64_t;
     using LinksOptionsType = LinksOptions<TLinkAddress>;
     using StorageType = UnitedMemoryLinks<LinksOptionsType, HeapResizableDirectMemory, LinksSourcesSizeBalancedTreeMethods<LinksOptionsType>, LinksTargetsSizeBalancedTreeMethods<LinksOptionsType>, UnusedLinksListMethods<TLinkAddress>, Doublets::ILinks<LinksOptionsType>>;
     StorageType storage { HeapResizableDirectMemory{}};
