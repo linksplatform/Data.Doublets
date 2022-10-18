f04332a5e99a7fb1e9ef0b93907e1efa5ae95a50
Daulet Nassipkali <uralskdev@gmail.com>
2022-10-18 21:22:30 +0600

    Update setup-gitpod.sh

--- cpp/setup-gitpod.sh
+++ cpp/setup-gitpod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../..
+cd ..
 
 sudo apt install -y g++-10
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
