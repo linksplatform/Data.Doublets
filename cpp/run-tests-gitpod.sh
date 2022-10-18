44b000192840fb6b0fba1a21affad0a7216812ff
Daulet Nassipkali <uralskdev@gmail.com>
2022-10-18 22:23:41 +0600

    Create run-tests-gitpod.sh

---
+++ cpp/run-tests-gitpod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd cpp
+
+cmake_flags="-DCMAKE_BUILD_TYPE=Release -DLINKS_PLATFORM_TESTS=ON"
+cmake_build_dir="build"
+mkdir -p $cmake_build_dir && cd $cmake_build_dir
+conan install .. -pr=linksplatform --build=missing
+cmake .. $cmake_flags
+cmake --build .
+scan-build cmake --build .
+binaries=bin/*
+for binary in $binaries
+do
+   ./$binary
+done
