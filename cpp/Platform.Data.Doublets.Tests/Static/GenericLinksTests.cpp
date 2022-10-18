16f8fa0c466d872b1b5a0750d933987ec14da626
FreePhoenix888 <freephoenix888@gmail.com>
2022-10-18 15:39:11 +0500

    Pass one memory to UnitedMemoryLinks

--- cpp/Platform.Data.Doublets.Tests/Static/GenericLinksTests.cpp
+++ cpp/Platform.Data.Doublets.Tests/Static/GenericLinksTests.cpp
@@ -5,7 +5,7 @@
     {
       using namespace Platform::Memory;
       using namespace Platform::Data::Doublets::Memory::Split::Generic;
-      TStorage storage{ HeapResizableDirectMemory{ }, HeapResizableDirectMemory{ } };
+      TStorage storage{ HeapResizableDirectMemory{ } };
       action(storage);
     }
 
