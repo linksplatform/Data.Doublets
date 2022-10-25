470173616c88890d7825d38ddee0530d555e2c50
FreePhoenix888 <freephoenix888@gmail.com>
2022-10-25 16:11:11 +0600

    Pass LinksOptions instead of TLinkAddress to UnusedLinksListMethods

--- cpp/Platform.Data.Doublets/Memory/United/Generic/UnitedMemoryLinks.h
+++ cpp/Platform.Data.Doublets/Memory/United/Generic/UnitedMemoryLinks.h
@@ -7,7 +7,7 @@
         typename TMemory = FileMappedResizableDirectMemory,
         typename TSourceTreeMethods = LinksSourcesSizeBalancedTreeMethods<TLinksOptions>,
         typename TTargetTreeMethods = LinksTargetsSizeBalancedTreeMethods<TLinksOptions>,
-        typename TUnusedLinks = UnusedLinksListMethods<typename TLinksOptions::LinkAddressType>,
+        typename TUnusedLinks = UnusedLinksListMethods<TLinksOptions>,
         typename ...TBase>
     struct UnitedMemoryLinks : public UnitedMemoryLinksBase<UnitedMemoryLinks<TLinksOptions, TMemory, TSourceTreeMethods, TTargetTreeMethods, TUnusedLinks, TBase...>, TLinksOptions, TMemory, TSourceTreeMethods, TTargetTreeMethods, TUnusedLinks, TBase...>, public std::enable_shared_from_this<UnitedMemoryLinks<TLinksOptions>>
     {
