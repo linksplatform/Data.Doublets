ee8ef5515178c98c660de486c5fec80af2c4ad41
FreePhoenix888 <freephoenix888@gmail.com>
2023-01-10 14:05:58 +0600

    Add extensions

--- csharp/Platform.Data.Doublets/ILinksExtensions.cs
+++ csharp/Platform.Data.Doublets/ILinksExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Platform.Ranges;
 using Platform.Collections.Lists;
 using Platform.Random;
@@ -1340,5 +1341,242 @@ namespace Platform.Data.Doublets
         /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Format<TLinkAddress>(this ILinks<TLinkAddress> links, TLinkAddress link)  where TLinkAddress : IUnsignedNumber<TLinkAddress>{return links.Format(links.GetLink(link));}
+        
+        /// <summary>
+        /// <para>
+        /// Determines whether any link is any.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="links">
+        /// <para>The links.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="sequence">
+        /// <para>The sequence.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The bool</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool AnyLinkIsAny<TLinkAddress>(this ILinks<ulong> links, params ulong[] sequence) where TLinkAddress: IUnsignedNumber<TLinkAddress>
+        {
+            if (sequence == null)
+            {
+                return false;
+            }
+            var constants = links.Constants;
+            for (var i = 0; i < sequence.Length; i++)
+            {
+                if (sequence[i] == constants.Any)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Formats the structure using the specified links.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="links">
+        /// <para>The links.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="linkIndex">
+        /// <para>The link index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="isElement">
+        /// <para>The is element.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderIndex">
+        /// <para>The render index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderDebug">
+        /// <para>The render debug.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The string</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatStructure<TLinkAddress>(this ILinks<ulong> links, ulong linkIndex, Func<Link<ulong>, bool> isElement, bool renderIndex = false, bool renderDebug = false) where TLinkAddress: IUnsignedNumber<TLinkAddress>
+        {
+            var sb = new StringBuilder();
+            var visited = new HashSet<ulong>();
+            links.AppendStructure<TLinkAddress>(sb, visited, linkIndex, isElement, (innerSb, link) => innerSb.Append(link.Index), renderIndex, renderDebug);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// <para>
+        /// Formats the structure using the specified links.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="links">
+        /// <para>The links.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="linkIndex">
+        /// <para>The link index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="isElement">
+        /// <para>The is element.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="appendElement">
+        /// <para>The append element.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderIndex">
+        /// <para>The render index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderDebug">
+        /// <para>The render debug.</para>
+        /// <para></para>
+        /// </param>
+        /// <returns>
+        /// <para>The string</para>
+        /// <para></para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FormatStructure<TLinkAddress>(this ILinks<ulong> links, ulong linkIndex, Func<Link<ulong>, bool> isElement, Action<StringBuilder, Link<ulong>> appendElement, bool renderIndex = false, bool renderDebug = false) where TLinkAddress: IUnsignedNumber<TLinkAddress>
+        {
+            var sb = new StringBuilder();
+            var visited = new HashSet<ulong>();
+            links.AppendStructure<TLinkAddress>(sb, visited, linkIndex, isElement, appendElement, renderIndex, renderDebug);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// <para>
+        /// Appends the structure using the specified links.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="links">
+        /// <para>The links.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="sb">
+        /// <para>The sb.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="visited">
+        /// <para>The visited.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="linkIndex">
+        /// <para>The link index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="isElement">
+        /// <para>The is element.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="appendElement">
+        /// <para>The append element.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderIndex">
+        /// <para>The render index.</para>
+        /// <para></para>
+        /// </param>
+        /// <param name="renderDebug">
+        /// <para>The render debug.</para>
+        /// <para></para>
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <para></para>
+        /// <para></para>
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AppendStructure<TLinkAddress>(this ILinks<ulong> links, StringBuilder sb, HashSet<ulong> visited, ulong linkIndex, Func<Link<ulong>, bool> isElement, Action<StringBuilder, Link<ulong>> appendElement, bool renderIndex = false, bool renderDebug = false) where TLinkAddress: IUnsignedNumber<TLinkAddress>
+        {
+            if (sb == null)
+            {
+                throw new ArgumentNullException(nameof(sb));
+            }
+            if (linkIndex == links.Constants.Null || linkIndex == links.Constants.Any || linkIndex == links.Constants.Itself)
+            {
+                return;
+            }
+            if (links.Exists(linkIndex))
+            {
+                if (visited.Add(linkIndex))
+                {
+                    sb.Append('(');
+                    var link = new Link<ulong>(links.GetLink(linkIndex));
+                    if (renderIndex)
+                    {
+                        sb.Append(link.Index);
+                        sb.Append(':');
+                    }
+                    if (link.Source == link.Index)
+                    {
+                        sb.Append(link.Index);
+                    }
+                    else
+                    {
+                        var source = new Link<ulong>(links.GetLink(link.Source));
+                        if (isElement(source))
+                        {
+                            appendElement(sb, source);
+                        }
+                        else
+                        {
+                            links.AppendStructure<TLinkAddress>(sb, visited, source.Index, isElement, appendElement, renderIndex);
+                        }
+                    }
+                    sb.Append(' ');
+                    if (link.Target == link.Index)
+                    {
+                        sb.Append(link.Index);
+                    }
+                    else
+                    {
+                        var target = new Link<ulong>(links.GetLink(link.Target));
+                        if (isElement(target))
+                        {
+                            appendElement(sb, target);
+                        }
+                        else
+                        {
+                            links.AppendStructure<TLinkAddress>(sb, visited, target.Index, isElement, appendElement, renderIndex);
+                        }
+                    }
+                    sb.Append(')');
+                }
+                else
+                {
+                    if (renderDebug)
+                    {
+                        sb.Append('*');
+                    }
+                    sb.Append(linkIndex);
+                }
+            }
+            else
+            {
+                if (renderDebug)
+                {
+                    sb.Append('~');
+                }
+                sb.Append(linkIndex);
+            }
+        }
+
     }
 }
