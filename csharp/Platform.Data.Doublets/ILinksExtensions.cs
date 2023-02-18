855affdbb59ca355de4185f30e04b88f903da833
FreePhoenix888 <freephoenix888@gmail.com>
2023-02-18 23:28:30 +0600

    Add ILinks extensions for manual garbage collection

--- csharp/Platform.Data.Doublets/ILinksExtensions.cs
+++ csharp/Platform.Data.Doublets/ILinksExtensions.cs
@@ -1572,5 +1572,31 @@ namespace Platform.Data.Doublets
             }
         }
 
+        #region Garbage Collection
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsGarbage<TLinkAddress>(this ILinks<TLinkAddress> links, TLinkAddress link) where  TLinkAddress : struct, IUnsignedNumber<TLinkAddress>, IComparisonOperators<TLinkAddress, TLinkAddress, bool> 
+        {
+            return !links.IsPartialPoint(link) && links.Count(links.Constants.Any, link) == TLinkAddress.Zero && links.Count(links.Constants.Any, links.Constants.Any, link) == TLinkAddress.Zero;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ClearGarbage<TLinkAddress>(this ILinks<TLinkAddress> links, TLinkAddress link) where TLinkAddress : struct, IUnsignedNumber<TLinkAddress>, IComparisonOperators<TLinkAddress, TLinkAddress, bool>
+        {
+            ClearGarbage(links, link, links.IsGarbage);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ClearGarbage<TLinkAddress>(this ILinks<TLinkAddress> links, TLinkAddress link, Func<TLinkAddress, bool> getIsGarbage) where  TLinkAddress : struct, IUnsignedNumber<TLinkAddress>, IComparisonOperators<TLinkAddress, TLinkAddress, bool> 
+        {
+            if (getIsGarbage(link))
+            {
+                var contents = new Link<TLinkAddress>(links.GetLink(link));
+                links.Delete(link);
+                links.ClearGarbage(contents.Source);
+                links.ClearGarbage(contents.Target);
+            }
+        }
+
+        #endregion
     }
 }
