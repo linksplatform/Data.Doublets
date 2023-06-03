96780ae14b79d6c3a8b53b3ab2f3fe0316c736fd
Konstantin Dyachenko <drakonard@gmail.com>
2023-06-03 20:48:21 +0000

    Setup script is updated.

--- cpp/setup-gitpod.sh
+++ cpp/setup-gitpod.sh
@@ -2,8 +2,7 @@
 
 cd ..
 
-sudo apt install -y g++-10
-sudo apt install cmake
+sudo apt install -y g++-10 cmake
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
 
 pip install conan
