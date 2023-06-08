15cbc6fe0b9f91a7a619b2a4cb77a2bef370d623
Daulet Nassipkali <uralskdev@gmail.com>
2023-06-08 19:10:10 +0500

    Update setup-gitpod.sh

--- cpp/setup-gitpod.sh
+++ cpp/setup-gitpod.sh
@@ -2,16 +2,14 @@
 
 cd ..
 
-sudo apt install -y g++-10 cmake
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+sudo apt install -y cmake
 
 pip install conan==1.53
 
 conan profile new linksplatform --detect
 conan profile update settings.compiler=gcc linksplatform
-conan profile update settings.compiler.version=10 linksplatform
+conan profile update settings.compiler.version=12 linksplatform
 conan profile update settings.compiler.libcxx=libstdc++11 linksplatform
-conan profile update env.CXX=g++-10 linksplatform
 conan profile show linksplatform
 
 git clone https://github.com/linksplatform/conan-center-index
