ba74dc457b745900bc7f054d96e511962910f273
Daulet Nassipkali <uralskdev@gmail.com>
2023-07-11 00:24:35 +0500

    Update install-local-conan-dependencies.sh

--- cpp/install-local-conan-dependencies.sh
+++ cpp/install-local-conan-dependencies.sh
@@ -12,7 +12,7 @@ conan create platform.random/all --version 0.2.0
 conan create platform.equality/all --version 0.0.1
 conan create platform.collections/all --version 0.2.1
 conan create platform.setters/all --version 0.1.0
-conan create platform.data/all --version 0.1.0
+conan create platform.data/all --version 0.1.1
 conan create platform.numbers/all --version 0.1.0
 conan create platform.memory/all --version 0.1.0
 conan create platform.collections.methods/all --version 0.3.0
